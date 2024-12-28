c29ae7fb5865403b642f7cb39ac78153ec4ef350
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-29 01:22:56 +1000

    [feat](header): enhance header with shopping bag and sign-in buttons

--- component/shared/header/header.tsx
+++ component/shared/header/header.tsx
@@ -1,20 +1,30 @@
 import Image from 'next/image'
 import Link from 'next/link'
-// import { ShoppingBagIcon, UserIcon } from 'lucide-react'
-// import { Button } from 'component/ui'
-import { GLOBAL, ICON_DIR } from 'config'
+import { ShoppingBagIcon, User2Icon } from 'lucide-react'
+import { Button } from 'component/ui'
+import { GLOBAL, ICON_DIR, PATH_DIR } from 'config'
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start ml-4">
-            <div className="bg-red-800">
-              <Image src={ICON_DIR.LOGO} alt="logo" width={24} height={24} priority />
-            </div>
-            <span className="hidden lg:block font-bold text-2xl ml-3">{GLOBAL.APP_NAME}</span>
+          <Link href="/" className="flex-start">
+            <Image src={ICON_DIR.LOGO} alt="logo" width={24} height={24} priority />
+            <span className="hidden lg:block ml-3">{GLOBAL.APP_NAME}</span>
           </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href={PATH_DIR.CART}>
+              <ShoppingBagIcon /> Bag
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href={PATH_DIR.SIGN_IN}>
+              <User2Icon /> Sign In
+            </Link>
+          </Button>
         </div>
       </div>
     </header>
