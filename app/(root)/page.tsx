f68bbb77b09fc98ac37ede27d3550de3e938f5ab
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-28 21:26:40 +1000

    feat(layout): add Header component to RootLayout and create Homepage component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+const Homepage = () => {
+  return (
+    <Fragment>
+      <h1 className="text-4xl font-bold">Homepage</h1>
+      <h6 className="mt-4">This is the homepage.</h6>
+    </Fragment>
+  )
+}
+
+export default Homepage
