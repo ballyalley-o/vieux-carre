f21d481dc7cd7b4e4340323c96bf0c71e52382ed
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-29 23:46:35 +1000

    ✨[feat](header): update logo source based on current theme for improved visual consistency

--- component/shared/header/header.tsx
+++ component/shared/header/header.tsx
@@ -2,17 +2,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { ShoppingBagIcon, User2Icon } from 'lucide-react'
+import { useTheme } from 'next-themes'
 import { ThemeToggle } from 'component'
 import { Button } from 'component/ui'
 import { GLOBAL, ICON_DIR, PATH_DIR } from 'config'
 
 const Header = () => {
+  const { theme } = useTheme()
+  const logoModes = theme === 'dark' ? ICON_DIR.LOGO_DARK : theme === 'light' ? ICON_DIR.LOGO : ICON_DIR.LOGO_RED
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
-            <Image src={ICON_DIR.LOGO} alt="logo" width={24} height={24} priority />
+            <Image src={logoModes} alt="logo" width={24} height={24} priority />
             <span className="hidden lg:block ml-3">{GLOBAL.APP_NAME}</span>
           </Link>
         </div>
