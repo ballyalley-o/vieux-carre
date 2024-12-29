57938e17fd4894d059883b198cc915e8f7e2549c
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-29 23:46:49 +1000

    add additional logo variants for improved theme support

--- config/dir/icon.tsx
+++ config/dir/icon.tsx
@@ -1,4 +1,7 @@
 export const ICON_DIR = {
   LOGO: '/image/svg/vieux-carre.svg',
+  LOGO_DARK: '/image/svg/vieux-carre-white.svg',
+  LOGO_RED: '/image/svg/vieux-carre-red.svg',
+  LOGO_GRAY: '/image/svg/vieux-carre-grey.svg',
   TEST: '/image/svg/logo.svg'
 }
