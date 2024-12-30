bc7033fab64f66c4ca39cb20ab6b4db95101248c
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-31 00:41:55 +1000

    (path): add mock paths for sample data and product slug

--- config/dir/path.tsx
+++ config/dir/path.tsx
@@ -1,6 +1,8 @@
 import { join } from 'lib'
 
 export const PATH_DIR = {
+  MOCK: join('__mock', 'sample-data.ts'),
+  MOCK_PRODUCT: (slug: string) => join('product', slug),
   BAG: join('bag'),
   SIGN_IN: join('sign-in')
 }
