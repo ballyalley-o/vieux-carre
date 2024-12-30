9a91cc91c606b00cc051c1490baab014cc71362a
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-31 00:42:07 +1000

    [interface](interface): add global TypeScript definitions for Product interface

---
+++ interface/global.d.ts
@@ -0,0 +1,16 @@
+declare global {}
+
+declare interface Product {
+  name: string
+  slug: string
+  category: string
+  description: string
+  images: string[]
+  price: number
+  brand: string
+  rating: number
+  numReviews: number
+  stock: number
+  isFeatured: boolean
+  banner: string | null
+}
