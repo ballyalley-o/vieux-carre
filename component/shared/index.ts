3a57b238971736de2823ee3ea7bc7b5c48f488ff
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-30 16:56:14 +1000

    [exports]: export Toolbar component from shared index

--- component/shared/index.ts
+++ component/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './header/header'
 export { default as ThemeToggle } from './header/theme-toggle'
+export { default as Toolbar } from './header/toolbar'
 export { default as Footer } from './footer/footer'
