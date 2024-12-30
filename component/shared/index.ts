fbf2ba58a7d1a5c375b74ef76dcbfd05b4fff305
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-31 00:42:30 +1000

    ✨ [feat](product): add ProductList component for displaying a list of products

--- component/shared/index.ts
+++ component/shared/index.ts
@@ -1,5 +1,10 @@
+// ⎿header
 export { default as Header } from './header/header'
-export { default as ThemeToggle } from './header/theme-toggle'
 export { default as Toolbar } from './header/toolbar'
+export { default as ThemeToggle } from './header/theme-toggle'
+export { default as MobileMenu } from './header/mobile-menu'
+// ⎿ product
+export { default as ProductList } from './product/product-list'
+// ⎿ footer
 export { default as Footer } from './footer/footer'
 export { default as LinkButton } from './button/link-button'
---
+++ component/shared/product/product-list.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+
+interface ProductListProps {
+  data: Product[]
+  title?: string
+  limit?: number
+}
+const ProductList: FC<ProductListProps> = ({ data, title, limit }) => {
+  const limitedData = limit ? data.slice(0, limit) : data
+
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product, index) => (
+            <div key={index}> {product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div className="">
+          <h3>{'No Products found'}</h3>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProductList
