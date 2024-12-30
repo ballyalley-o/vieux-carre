f62ee862e0bd12788bb7f45773e05e4370dbb64c
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-31 00:42:40 +1000

    ✨ [feat](homepage): integrate ProductList component to display newest arrivals on the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
 import { Fragment } from 'react'
+import { _mockData } from '__mock'
+import { ProductList } from 'component'
 const Homepage = () => {
   return (
     <Fragment>
+      <ProductList data={_mockData.products} title={'Newest Arrivals'} limit={4} />
       <h1 className="text-8xl font-bold">Homepage</h1>
       <h6 className="mt-4">This is the homepage.</h6>
     </Fragment>
