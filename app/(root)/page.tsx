dc3820bd2968cbe9b9033073156c53c3191aeded
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-12-31 11:36:25 +1000

    update homepage title to 'Shop.Go.'

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <Fragment>
       <ProductList data={_mockData.products} title={'Newest Arrivals'} limit={4} />
-      <h1 className="text-8xl font-bold">Homepage</h1>
+      <h1 className="text-8xl font-bold">{'Shop.Go.'}</h1>
       <h6 className="mt-4">This is the homepage.</h6>
     </Fragment>
   )
