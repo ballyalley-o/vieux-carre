ee9e3c2238463926ee2e424595bfd392b32b9f1c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 23:22:12 +1000

    [interface]: import ProductSchema and extend Product interface with rating property

--- interface/global.d.ts
+++ interface/global.d.ts
@@ -1,23 +1,12 @@
 import { z } from 'zod'
+import { ProductSchema } from 'lib'
 
 declare global {}
-
-export const ProductSchema = z.object({
-  name: z.string().min(3, 'Name must be at least 3 characters').max(255),
-  slug: z.string().min(5, 'Slug must be at least 5 characters').max(255),
-  category: z.string().min(5, 'Category must be at least 5 characters').max(255),
-  brand: z.string().min(5, 'Brand must be at least 5 characters').max(255),
-  description: z.string().min(5, 'Slug must be at least 5 characters').max(255),
-  stock: z.coerce.number(),
-  images: z.array(z.string()).min(1, 'Product must be at least 1 image'),
-  isFeatured: z.boolean(),
-  banner: z.string().nullable()
-})
 
 declare global {
   export interface Product extends z.infer<typeof ProductSchema> {
     id: number
+    rating: string
     createdAt: Date
-    updatedAt: Date
   }
 }
