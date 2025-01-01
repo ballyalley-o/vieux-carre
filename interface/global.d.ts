8f2ff3a714cff9dbbfe8df9211322a7c7ffe5a03
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 20:54:21 +1000

    ✨ [validation-interface]: define Product schema using Zod and extend Product interface

--- interface/global.d.ts
+++ interface/global.d.ts
@@ -1,16 +1,23 @@
+import { z } from 'zod'
+
 declare global {}
 
-declare interface Product {
-  name: string
-  slug: string
-  category: string
-  description: string
-  images: string[]
-  price: number
-  brand: string
-  rating: number
-  numReviews: number
-  stock: number
-  isFeatured: boolean
-  banner: string | null
+export const ProductSchema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters').max(255),
+  slug: z.string().min(5, 'Slug must be at least 5 characters').max(255),
+  category: z.string().min(5, 'Category must be at least 5 characters').max(255),
+  brand: z.string().min(5, 'Brand must be at least 5 characters').max(255),
+  description: z.string().min(5, 'Slug must be at least 5 characters').max(255),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, 'Product must be at least 1 image'),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable()
+})
+
+declare global {
+  export interface Product extends z.infer<typeof ProductSchema> {
+    id: number
+    createdAt: Date
+    updatedAt: Date
+  }
 }
