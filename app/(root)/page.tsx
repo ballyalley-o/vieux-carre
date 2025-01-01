a885580c48e6dd77ec1c82c971dbc88ddce7bfbc
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 20:52:49 +1000

    fetch latest products for the homepage and update ProductList component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from 'react'
-import { _mockData } from '__mock'
+import { getLatestProducts } from 'lib'
 import { ProductList } from 'component'
-const Homepage = () => {
+import { GLOBAL } from 'config'
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts()
   return (
     <Fragment>
-      <ProductList data={_mockData.products} title={'Newest Arrivals'} limit={4} />
+      <ProductList data={latestProducts} title={'Newest Arrivals'} limit={GLOBAL.LATEST_PRODUCT_QUANTITY} />
       <h1 className="text-8xl font-bold">{'Shop.Go.'}</h1>
       <h6 className="mt-4">This is the homepage.</h6>
     </Fragment>
