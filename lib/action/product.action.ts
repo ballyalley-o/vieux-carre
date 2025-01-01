b878115a3cf6f40ef2fb0d4c62fd660d37a32c47
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 23:21:22 +1000

    [action]: refactor to use shared Prisma client and add getProductBySlug function

--- lib/action/product.action.ts
+++ lib/action/product.action.ts
@@ -1,11 +1,13 @@
 'use server'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from 'db/prisma'
 import { GLOBAL } from 'config'
 import { convertToPlainObject } from 'lib'
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient()
   const data = await prisma.product.findMany({ take: GLOBAL.LATEST_PRODUCT_QUANTITY, orderBy: { createdAt: 'desc' } })
-
   return convertToPlainObject(data)
+}
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({ where: { slug } })
 }
