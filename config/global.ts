54e811b5166458730d612c5b8a74383d98e61f69
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 20:54:05 +1000

    🔧 [config]: add LATEST_PRODUCT_QUANTITY to global configuration

--- config/global.ts
+++ config/global.ts
@@ -2,5 +2,6 @@ export const GLOBAL = {
   APP_NAME: process.env.NEXT_PUBLIC_APP_NAME || 'Vieux Carré',
   APP_VERSION: process.env.NEXT_PUBLIC_APP_VERSION || '1.0.0',
   APP_DESCRIPTION: process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Old Square shop application',
-  SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+  SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
+  LATEST_PRODUCT_QUANTITY: 4
 }
