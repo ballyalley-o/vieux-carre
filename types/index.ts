02ee2b8565c5ccd69f14b52348cebb795ba7cdab
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 20:18:51 +1000

    🦄 [refax] add initial TypeScript types for Product interface with Zod schema integration; typings -> types

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+// import { z } from 'zod'
+// import { ProductSchema } from 'lib/schema'
+
+// export interface Product extends z.infer<typeof ProductSchema> {
+//   id: number
+//   createdAt: Date
+//   updatedAt: Date
+// }
