106b8313951750bdd7c0395b4bf7e4bb229d8396
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 23:21:37 +1000

    [validation]: enhance Product schema to validate price with two decimal places

--- lib/schema/product-schema.ts
+++ lib/schema/product-schema.ts
@@ -1,4 +1,7 @@
 import { z } from 'zod'
+import { formatNumberWithDecimal } from 'lib/util'
+
+const currency = z.string().refine((num) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(num))), 'Price must have exactly 2 decimal places')
 
 export const ProductSchema = z.object({
   name: z.string().min(3, 'Name must be at least 3 characters').max(255),
@@ -9,5 +12,6 @@ export const ProductSchema = z.object({
   stock: z.coerce.number(),
   images: z.array(z.string()).min(1, 'Product must be at least 1 image'),
   isFeatured: z.boolean(),
-  banner: z.string().nullable()
+  banner: z.string().nullable(),
+  price: currency
 })
