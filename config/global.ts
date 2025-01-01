eb2a6b13d09a4b7978e582db033d3724b2c2e067
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-01 23:21:49 +1000

    🔧 [config]: add DATABASE_URL to global configuration

--- config/global.ts
+++ config/global.ts
@@ -3,5 +3,6 @@ export const GLOBAL = {
   APP_VERSION: process.env.NEXT_PUBLIC_APP_VERSION || '1.0.0',
   APP_DESCRIPTION: process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Old Square shop application',
   SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
+  DATABASE_URL: process.env.DATABASE_URL || '',
   LATEST_PRODUCT_QUANTITY: 4
 }
