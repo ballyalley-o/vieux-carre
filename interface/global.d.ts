b4a20eb58ca0a703cde85bde766b63d71fa6de1d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-02 23:46:06 +1000

    [interface]: extend global types with ReadonlyReactNode interface

--- interface/global.d.ts
+++ interface/global.d.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod'
 import { ProductSchema } from 'lib'
 
-declare global {}
-
 declare global {
   export interface Product extends z.infer<typeof ProductSchema> {
     id: number
     rating: string
     createdAt: Date
+  }
+
+  export interface ReadonlyReactNode {
+    children: Readonly<{ children: ReactNode }>
   }
 }
