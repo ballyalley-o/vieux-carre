684e4378e84cdca17f663bb658f58ef5aa9854e6
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-03 00:27:33 +1000

    [constant]: update ERROR response structure and add DEFAULT response

--- lib/constant/response.ts
+++ lib/constant/response.ts
@@ -8,5 +8,6 @@ export const RESPONSE = {
    * @returns  { code: number, success: boolean, message: string }
    */
   SUCCESS: (message: string, code: CODE = CODE.OK) => ({ code, success: true, message }),
-  ERROR: (error: string, code = CODE.INTERNAL_SERVER_ERROR) => ({ code, success: false, error })
+  ERROR: (message: string, code = CODE.INTERNAL_SERVER_ERROR) => ({ code, success: false, message }),
+  DEFAULT: { code: CODE.INTERNAL_SERVER_ERROR, success: false, message: '' }
 }
