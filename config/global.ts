64a8c71bca16f101bf8792a162e859803add1146
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-02 19:19:37 +1000

    🔧 [config]: add NEXTAUTH_STRATEGY configuration with default value 'jwt'

--- config/global.ts
+++ config/global.ts
@@ -4,5 +4,6 @@ export const GLOBAL = {
   APP_DESCRIPTION: process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Old Square shop application',
   SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL || '',
+  NEXTAUTH_STRATEGY: process.env.NEXTAUTH_STRATEGY || 'jwt',
   LATEST_PRODUCT_QUANTITY: 4
 }
