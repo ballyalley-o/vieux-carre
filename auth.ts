babfe0f527b405df04e8f6afe25266ac08dd935e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-02 19:20:27 +1000

    🔧 [config]: implement NextAuth configuration with credentials provider and session management

---
+++ auth.ts
@@ -0,0 +1,61 @@
+import type { NextAuthConfig } from 'next-auth'
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { prisma } from 'db/prisma'
+import { GLOBAL, thirtyDays } from 'config'
+import { compareSync } from 'bcrypt-ts-edge'
+
+export type SessionStrategyType = 'jwt' | 'database' | undefined
+
+export const config = {
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in'
+  },
+  session: {
+    strategy: GLOBAL.NEXTAUTH_STRATEGY as SessionStrategyType,
+    maxAge: thirtyDays
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' }
+      },
+      async authorize(credentials) {
+        if (credentials === null) return null
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string
+          }
+        })
+        if (user && user.password) {
+          const isMatch = compareSync(credentials.password as string, user.password)
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role
+            }
+          }
+        }
+        return null
+      }
+    })
+  ],
+  callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async session({ session, user, trigger, token }: any) {
+      session.user.id = token.sub
+      if (trigger === 'update') {
+        session.user.name = user.name
+      }
+      return session
+    }
+  }
+} satisfies NextAuthConfig
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config)
