0639dd8ba7b8623d735eee94c5ecd09893ef1f93
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-02 23:46:17 +1000

    [constant]: add default values for sign-in form and update constants

--- lib/constant/index.ts
+++ lib/constant/index.ts
@@ -1,3 +1,4 @@
-export * from './key'
 export * from './code'
+export * from './form-default-value'
+export * from './key'
 export * from './response'
--- lib/constant/key.ts
+++ lib/constant/key.ts
@@ -7,12 +7,18 @@ export const VARIANT = {
   GHOST: 'ghost'
 }
 
+export const FORM = {
+  EMAIL: 'email',
+  PASSWORD: 'password'
+}
+
 export const KEY = {
   BAG: 'bag',
   CLASS: 'class',
   SYSTEM: 'system',
   LIGHT: 'light',
   DARK: 'dark',
+  ...FORM,
   ...LOCALE,
   ...VARIANT
 }
