6d0e03ce302d015ea809d6937fc1c07126b90bbb
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-02 13:27:30 +1000

    [db]: update pre-load script to clear and seed account, session, verificationToken, and user data

--- __mock/pre-load.ts
+++ __mock/pre-load.ts
@@ -5,7 +5,12 @@ import { _mockData } from '__mock'
 async function main() {
   const prisma = new PrismaClient()
   await prisma.product.deleteMany()
+  await prisma.account.deleteMany()
+  await prisma.session.deleteMany()
+  await prisma.verificationToken.deleteMany()
+  await prisma.user.deleteMany()
   await prisma.product.createMany({ data: _mockData.products })
+  await prisma.user.createMany({ data: _mockData.users })
 
   goodlog.log(' 🌱 Data pre-loaded')
 }
