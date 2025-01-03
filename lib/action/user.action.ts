6c598888b7eb9053d798a69337bc969877aff5fc
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-04 08:41:53 +1000

    ✨[feat]: implement user sign-up functionality with password hashing

--- lib/action/user.action.ts
+++ lib/action/user.action.ts
@@ -1,5 +1,8 @@
 'use server'
-import { SignInSchema } from 'lib/schema'
+
+import { hashSync } from 'bcrypt-ts-edge'
+import { SignInSchema, SignUpSchema } from 'lib/schema'
+import { prisma } from 'db/prisma'
 import { signIn, signOut } from 'auth'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { CODE, RESPONSE } from 'lib/constant'
@@ -23,4 +26,25 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 
 export async function signOutUser() {
   await signOut()
+}
+
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = SignUpSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword')
+    })
+    const unhashedPassword = user.password
+    user.password = hashSync(user.password, 10)
+    await prisma.user.create({ data: { name: user.name, email: user.email, password: user.password } })
+    await signIn('credentials', { email: user.email, password: unhashedPassword })
+    return RESPONSE.SUCCESS('User Signed Up')
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+    return RESPONSE.ERROR('User failed to sign up', CODE.BAD_REQUEST)
+  }
 }
