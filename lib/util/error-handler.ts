1d6672c8fc2799b17a33be78fae45e1d60dbce28
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-08 01:24:30 +1300

    add error handler utility for improved error management

---
+++ lib/util/error-handler.ts
@@ -0,0 +1,14 @@
+import { Prisma } from '@prisma/client'
+import { ZodError } from 'zod'
+
+export const errorHandler = (error: AppError) => {
+  if (error instanceof ZodError) {
+    const errors = error.errors.map((err) => err.message)
+    return errors.join(', ')
+  } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    const field = (error.meta as { target?: string[] })?.target ? (error.meta as { target?: string[] }).target![0] : 'Field'
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`
+  } else {
+    return typeof error.message === 'string' ? error.message : JSON.stringify(error.message)
+  }
+}
