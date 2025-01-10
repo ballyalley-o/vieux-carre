c53ace850db258ae113a097cc19b0efc5ff57522
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-11 05:00:44 +1300

    [format] : align global configuration constants for improved readability

--- config/global.ts
+++ config/global.ts
@@ -1,13 +1,14 @@
+// prettier-ignore
 export const GLOBAL = {
-  APP_NAME: process.env.NEXT_PUBLIC_APP_NAME || 'Vieux Carré',
-  APP_VERSION: process.env.NEXT_PUBLIC_APP_VERSION || '1.0.0',
-  APP_DESCRIPTION: process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Old Square shop application',
-  SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || '',
-  NEXTAUTH_STRATEGY: process.env.NEXTAUTH_STRATEGY || 'jwt',
+  APP_NAME               : process.env.NEXT_PUBLIC_APP_NAME || 'Vieux Carré',
+  APP_VERSION            : process.env.NEXT_PUBLIC_APP_VERSION || '1.0.0',
+  APP_DESCRIPTION        : process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Old Square shop application',
+  SERVER_URL             : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
+  DATABASE_URL           : process.env.DATABASE_URL || '',
+  NEXTAUTH_STRATEGY      : process.env.NEXTAUTH_STRATEGY || 'jwt',
   LATEST_PRODUCT_QUANTITY: 4,
-  PRICES: {
-    TAX: process.env.NEXT_PUBLIC_TAX || 0.15,
+  PRICES                 : {
+    TAX                  : process.env.NEXT_PUBLIC_TAX || 0.15,
     NO_SHIPPING_THRESHOLD: process.env.NEXT_PUBLIC_NO_SHIPPING_THRESHOLD || 100,
     DEFAULT_SHIPPING_COST: process.env.NEXT_PUBLIC_DEFAULT_SHIPPING_COST || 10
   }
