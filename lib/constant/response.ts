20f04e8401275dce10abc2c8341b726044fa7120
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-11 05:00:00 +1300

    [prettierrc]: align response constants for improved readability

--- lib/constant/response.ts
+++ lib/constant/response.ts
@@ -8,8 +8,8 @@ export const RESPONSE = {
    * @param message - message to be displayed
    * @returns  { code: number, success: boolean, message: string }
    */
-  SUCCESS: (message: string, code: CODE = CODE.OK) => ({ code, success: true, message }),
-  ERROR: (message: string, code = CODE.INTERNAL_SERVER_ERROR) => ({ code, success: false, message }),
+  SUCCESS        : (message: string, code: CODE = CODE.OK) => ({ code, success: true, message }),
+  ERROR          : (message: string, code = CODE.INTERNAL_SERVER_ERROR) => ({ code, success: false, message }),
   ERROR_FORMATTED: (message: AppError, code = CODE.INTERNAL_SERVER_ERROR) => ({ code, success: false, message: errorHandler(message) }),
-  DEFAULT: { code: CODE.INTERNAL_SERVER_ERROR, success: false, message: '' }
+  DEFAULT        : { code: CODE.INTERNAL_SERVER_ERROR, success: false, message: '' }
 }
