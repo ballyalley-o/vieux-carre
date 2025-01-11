68d6fc3d3642c03746a318237b14c53fe4f9025f
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-12 01:41:57 +1300

    [ui]: update ProductImage component to change border style and add scaling effect for non-active images

--- component/shared/product/product-image.tsx
+++ component/shared/product/product-image.tsx
@@ -17,8 +17,8 @@ const ProductImage: FC<ProductImageProps> = ({ images }) => {
           <div
             key={index}
             onClick={() => setCurrent(index)}
-            className={cn('border mr-2 cursor-pointer hover:border-gray-700', current === index && 'border-gray-400')}>
-            <Image src={image} alt={'product-image'} width={80} height={80} />
+            className={cn('border-b-4 mr-2 cursor-pointer hover:border-gray-700', current === index && 'border-gray-400')}>
+            <Image src={image} alt={'product-image'} width={80} height={80} className={cn(current !== index && 'scale-75')} />
           </div>
         ))}
       </div>
