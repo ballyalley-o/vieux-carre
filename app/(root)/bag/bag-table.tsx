5261c33acad2311c3e9eb05c0c0d3553556456ee
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-12 03:24:33 +1300

    update bag table to include subtotal and checkout button

--- app/(root)/bag/bag-table.tsx
+++ app/(root)/bag/bag-table.tsx
@@ -3,11 +3,13 @@
 import { FC, JSX, ReactNode, Fragment, useTransition } from 'react'
 import { en } from 'public/locale'
 import Link from 'next/link'
-// import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { useToast } from 'hook'
 import { addItemToBag, removeItemFromBag } from 'lib/action'
-import { Table} from 'component/ui'
+import { ArrowRight, Loader } from 'lucide-react'
+import { Table, Card, CardContent, Button } from 'component/ui'
 import { PATH_DIR } from 'config'
+import { formatCurrency } from 'lib/util'
 import BagTableHead from './bag-table-head'
 import BagTableBody from './bag-table-body'
 
@@ -29,7 +31,9 @@ interface BagTableProps {
 const BagTable: FC<BagTableProps> = ({ bag }) => {
   const [isPending, startTransition] = useTransition()
   const { toast } = useToast()
-  // const router = useRouter()
+  const router = useRouter()
+
+  const subtotal = bag.items.reduce((acc, item) => acc + item.qty, 0)
 
   const handleRemoveItem = async (item: BagItem) => {
     startTransition(async () => {
@@ -49,9 +53,21 @@ const BagTable: FC<BagTableProps> = ({ bag }) => {
     })
   }
 
+  const handleNavigateCheckout = () => {
+    startTransition(async () => router.push(PATH_DIR.SHIPPING))
+  }
+
+  const renderNavigateCheckoutButton = () => {
+    return (
+      <Button className="w-full" disabled={isPending} onClick={handleNavigateCheckout}>
+        {isPending ? <Loader className={'h-4 w-4 animate-spin'} /> : (<Fragment> <ArrowRight className='w-4 h-4' /> {'Proceed to Checkout'}</Fragment>)}
+      </Button>
+    )
+  }
+
   return (
     <Fragment>
-      <h1 className="py-4 h2-bold">{'Bag Table'}</h1>
+      <h1 className="py-4 h2-bold">{'Your Bag'}</h1>
       {!bag || bag.items.length === 0 ? (
         <div>
           {en.bag_empty} <Link href={PATH_DIR.ROOT}>{en.go_shopping}</Link>
@@ -64,6 +80,15 @@ const BagTable: FC<BagTableProps> = ({ bag }) => {
               <BagTableBody bag={bag} isPending={isPending} handleMinus={handleRemoveItem} handlePlus={handleAddItem} />
             </Table>
           </div>
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <div className="pb-3 text-xl">
+                {'Subtotal'} ({subtotal}):
+                <span className="font-bold">{formatCurrency(bag.itemsPrice)}</span>
+              </div>
+              {renderNavigateCheckoutButton()}
+            </CardContent>
+          </Card>
         </div>
       )}
     </Fragment>
