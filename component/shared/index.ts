059cf9a7056b3f2255b649fd52612bd0115ded5a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-12 01:42:06 +1300

    [refax]: reorganize exports in shared index file for better structure and clarity

--- component/shared/index.ts
+++ component/shared/index.ts
@@ -1,18 +1,20 @@
+// ⎿ bag
+export { default as AddToBag } from './bag/add-to-bag'
+// ⎿ btn
+export { default as LinkBtn } from './btn/link-btn'
+export { default as DynamicBagBtn } from './btn/dynamic-bag-btn'
+// ⎿ footer
+export { default as Footer } from './footer/footer'
 // ⎿header
 export { default as Header } from './header/header'
 export { default as Toolbar } from './header/toolbar'
 export { default as ThemeToggle } from './header/theme-toggle'
 export { default as MobileMenu } from './header/mobile-menu'
 export { default as UserMenu } from './header/user-menu'
+// ⎿ loader
+export { default as Loader } from './loader/loader'
 // ⎿ product
 export { default as ProductList } from './product/product-list'
 export { default as ProductCard } from './product/product-card'
 export { default as ProductPrice } from './product/product-price'
 export { default as ProductImage } from './product/product-image'
-// ⎿ footer
-export { default as Footer } from './footer/footer'
-export { default as LinkButton } from './button/link-button'
-// ⎿ loader
-export { default as Loader } from './loader/loader'
-// ⎿ bag
-export { default as AddToBag } from './bag/add-to-bag'
