f83ac5693c6cdce8dd9d9595936786d86a9d1b85
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-13 03:38:26 +1300

    implement route protection by defining PROTECTED_ROUTES based on global configuration

--- config/dir/path.tsx
+++ config/dir/path.tsx
@@ -1,5 +1,14 @@
 import { join } from 'lib'
+import { GLOBAL } from 'config'
 
+const wrapPath = (path: string) => `/${path}`
+
+const protect = () => {
+  const routesArray = GLOBAL.PROTECTED_ROUTES.split(';') || ""
+  return routesArray.map((route: string) => new RegExp(wrapPath(route)))
+}
+
+export const PROTECTED_ROUTES = protect()
 export const PATH_DIR = {
   BAG: join('bag'),
   MOCK: join('__mock', 'sample-data.ts'),
