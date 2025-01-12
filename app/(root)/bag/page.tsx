7b34d815bc434fdf4759693270dec5d2d1c9d6e3
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-12 23:29:59 +1300

    [type-infer]: add type annotation for metadata export

--- app/(root)/bag/page.tsx
+++ app/(root)/bag/page.tsx
@@ -1,8 +1,9 @@
 
+import { Metadata } from 'next'
 import { getMyBag } from "lib/action";
 import BagTable from "./bag-table";
 
-export const metadata = {
+export const metadata: Metadata = {
     title: 'Shopping Bag',
 }
 
