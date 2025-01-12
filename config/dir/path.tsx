bb1fc3a532dee9bec6686a378bd1141c44ca14ef
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-12 23:29:06 +1300

    [config]: update path constants to include payment and shipping routes

--- config/dir/path.tsx
+++ config/dir/path.tsx
@@ -1,10 +1,12 @@
 import { join } from 'lib'
 
 export const PATH_DIR = {
-  ROOT: '/',
-  MOCK: join('__mock', 'sample-data.ts'),
-  PRODUCT_VIEW: (slug: string) => join('product', slug),
   BAG: join('bag'),
+  MOCK: join('__mock', 'sample-data.ts'),
+  PAYMENT: join('payment'),
+  PRODUCT_VIEW: (slug: string) => join('product', slug),
+  ROOT: '/',
+  SHIPPING: join('shipping'),
   SIGN_IN: join('sign-in'),
   SIGN_UP: join('sign-up')
 }
