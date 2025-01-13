a53bcbc09fbdc9db926a4dcd24fc08c6daf5b551
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-13 23:50:21 +1300

    [schema]: add payment method schema and export it in the schema index

---
+++ lib/schema/payment-schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod"
+import { en } from "public/locale"
+import { GLOBAL } from "config"
+
+const PAYMENT_METHODS = GLOBAL.PAYMENT_METHODS && GLOBAL.PAYMENT_METHODS.split(';') || ['PayPal', 'Stripe', 'Cash']
+
+export const PaymentMethodSchema = z.object({
+    type: z.string().min(1, 'Payment method is required')
+}).refine((data) => PAYMENT_METHODS.includes(data.type), { path:['type'], message: en.error.invalid_payment_method })
