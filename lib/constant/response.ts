9c7372d3991921f36c3b3900792b47f048a7cb7a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-16 02:10:17 +1300

    [docs-refax]: added redirectTo to the success formatter

--- lib/constant/response.ts
+++ lib/constant/response.ts
@@ -2,14 +2,22 @@ import { CODE } from './code'
 import { errorHandler } from 'lib/util'
 
 export const RESPONSE = {
-  /*
-   * Success response
+  /**
+   * Success reponse formatter
+   * @param message
    * @param code - default 200
-   * @param message - message to be displayed
-   * @returns  { code: number, success: boolean, message: string }
+   * @param redirectTo - redirect path
+   * @returns  { code: number, success: boolean, message: string, redirectTo: string }
    */
-  SUCCESS        : (message: string, code: CODE = CODE.OK) => ({ code, success: true, message }),
-  ERROR          : (message: string, code = CODE.INTERNAL_SERVER_ERROR, redirectTo?: string) => ({ code, success: false, message, redirectTo}),
+  SUCCESS: (message: string, code: CODE = CODE.OK, redirectTo?: string) => ({ code, success: true, message, redirectTo }),
+  /**
+   * Error response formatter
+   * @param message
+   * @param code
+   * @param redirectTo
+   * @returns
+   */
+  ERROR: (message: string, code = CODE.INTERNAL_SERVER_ERROR, redirectTo?: string) => ({ code, success: false, message, redirectTo }),
   ERROR_FORMATTED: (message: AppError, code = CODE.INTERNAL_SERVER_ERROR) => ({ code, success: false, message: errorHandler(message) }),
-  DEFAULT        : { code: CODE.INTERNAL_SERVER_ERROR, success: false, message: '' }
+  DEFAULT: { code: CODE.INTERNAL_SERVER_ERROR, success: false, message: '' }
 }
