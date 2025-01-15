748bfd11f47b49c056870bb70b3eaa639e74537c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-16 03:33:19 +1300

    [locale]: update checkout button to show pending message and change icon

--- app/(root)/order/checkout/checkout-form.tsx
+++ app/(root)/order/checkout/checkout-form.tsx
@@ -4,7 +4,7 @@ import { en } from 'public/locale'
 import { useRouter } from 'next/navigation'
 import { useFormStatus } from 'react-dom'
 import { createOrder } from 'lib/action'
-import { Check, Loader } from 'lucide-react'
+import { Loader, Barcode } from 'lucide-react'
 import { Button } from 'component/ui'
 
 const CheckoutForm = () => {
@@ -19,8 +19,8 @@ const CheckoutForm = () => {
     const { pending } = useFormStatus()
     return (
       <Button disabled={pending} className={'w-full'}>
-        {pending ? <Loader className={'loader'} /> : <Check className={'default-size_icon'} />}
-       {en.place_order.label}
+        {pending ? <Loader className={'loader'} /> : <Barcode className={'default-size_icon'} />}
+        {pending ? <i>{en.place_order.pending}</i> : en.place_order.label}
       </Button>
     )
   }
