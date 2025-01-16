d016f7283527df0831a8445eb940ad51cd97aeb2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-17 01:11:47 +1300

    [config-paypal]: implement PayPal access token generation for enhanced payment processing

---
+++ lib/paypal.ts
@@ -0,0 +1,26 @@
+import { GLOBAL } from 'config/global'
+import { join } from 'path'
+
+const base = GLOBAL.PAYPAL.PAYPAL_API_URL
+export const paypal = {}
+
+export async function generateAccessToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = GLOBAL.PAYPAL
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString('base64')
+
+  const url = join(base, 'v1', 'oauth2', 'token')
+  console.log('URL: ', url)
+  const response = await fetch(url, {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: { Authorization: `Basic ${auth}`, 'Content-Type': 'application/x-www-form-urlencoded' }
+  })
+
+  if (response.ok) {
+    const jsonData = await response.json()
+    return jsonData.access_token
+  } else {
+    const errorMessage = await response.text()
+    throw new Error(errorMessage)
+  }
+}
