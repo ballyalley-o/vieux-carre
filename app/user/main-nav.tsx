d9acaf83323f6a581fa142466e88a77bd293c2aa
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-18 23:43:20 +1300

    [feat]: add MainNav component for user navigation; init

---
+++ app/user/main-nav.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { HTMLAttributes } from 'react'
+import Link from 'next/link'
+// import { en } from 'public/locale'
+import { usePathname } from 'next/navigation'
+import { cn } from 'lib'
+import { NAV_CONFIG } from 'config/nav-config'
+
+const MainNav = ({ className, ...props }: HTMLAttributes<HTMLElement>) => {
+  const pathname = usePathname()
+
+  return (
+    <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)} {...props}>
+      {NAV_CONFIG.map((item, index) => (
+        <Link
+          key={index}
+          href={item.href}
+          className={cn('text-sm font-medium transition-colors hover:text-primary', pathname.includes(item.href) ? '' : 'text-muted-foreground')}>
+          {item.title}
+        </Link>
+      ))}
+    </nav>
+  )
+}
+
+export default MainNav
