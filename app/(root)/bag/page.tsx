48ae1713d437a020ef40428be6ad1009d5b4927a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-19 22:35:00 +1300

    [code-styling]: simplify metadata declaration in bag page component

--- app/(root)/bag/page.tsx
+++ app/(root)/bag/page.tsx
@@ -3,9 +3,7 @@ import { Metadata } from 'next'
 import { getMyBag } from "lib/action";
 import BagTable from "./bag-table";
 
-export const metadata: Metadata = {
-    title: 'Shopping Bag',
-}
+export const metadata: Metadata = { title: 'Shopping Bag' }
 
 const BagPage = async () => {
     const bag = await getMyBag()
