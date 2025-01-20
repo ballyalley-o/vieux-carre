efbd3ff950dc5e2e0b888dd86fcb78d3286e9a1e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-21 01:05:20 +1300

    [uiux]: refactor UserLayout component for improved structure and styling

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -9,21 +9,19 @@ import MainNav from './main-nav'
 
 export default function UserLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="flex flex-col">
-      <div className="border-b container mx-auto">
-        <div className="flex items-center h-16 px-4">
+    <header className="flex  h-screen flex-col">
+      <header className="w-full border-b">
+        <div className="wrapper flex-center">
           <Link href={PATH_DIR.ROOT} className={'w-22'}>
             <Image src={ASSET_DIR.LOGO_RED} height={LOGO.NAV_ADMIN_H} width={LOGO.NAV_ADMIN_W} alt={GLOBAL.APP_NAME} />
           </Link>
-          <MainNav className={'mx-6'}/>
-          <div className="ml-auto items-center flex space-x-4">
+          <MainNav className={'mx-6'} />
+          <div className="ml-auto items-center flex">
             <Toolbar />
           </div>
         </div>
-      </div>
-      <div className="flex-1 space-y-4 p-8 pt-6 container mx-auto">
-        {children}
-      </div>
-    </div>
+      </header>
+      <div className="flex-1 space-y-4 p-8 pt-6 container mx-auto">{children}</div>
+    </header>
   )
 }
