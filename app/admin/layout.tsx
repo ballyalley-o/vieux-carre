6c76278796208d92d58078b65f04ce5e529ce240
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-21 01:46:41 +1300

    ✨ [feat]: add AdminLayout, MainNav, and AdminOverviewPage components for admin interface

---
+++ app/admin/layout.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import { GLOBAL } from 'vieux-carre'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Input } from 'component/ui'
+import { Toolbar } from 'component/shared'
+import { ASSET_DIR, PATH_DIR } from 'config'
+import { LOGO } from 'config/layout'
+import MainNav from './main-nav'
+
+export default function AdminLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <header className="flex  h-screen flex-col">
+      <header className="w-full border-b">
+        <div className="wrapper flex-center">
+          <Link href={PATH_DIR.ROOT} className={'w-22'}>
+            <Image src={ASSET_DIR.LOGO_BLK} height={LOGO.NAV_ADMIN_H} width={LOGO.NAV_ADMIN_W} alt={GLOBAL.APP_NAME} />
+          </Link>
+          <MainNav className={'mx-6'} />
+          <div className="ml-auto items-center flex">
+            <div>
+              <Input type={'search'} placeholder={'Search...'} className={'md:x-[100px] lg:w-[300px]'} />
+            </div>
+            <Toolbar />
+          </div>
+        </div>
+      </header>
+      <div className="flex-1 space-y-4 p-8 pt-6 container mx-auto">{children}</div>
+    </header>
+  )
+}
---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,7 @@
+import { Fragment } from "react";
+
+const AdminOverviewPage = () => {
+    return ( <Fragment>{'Admin overview page'}</Fragment> );
+}
+
+export default AdminOverviewPage;
