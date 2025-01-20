86859b98b55b6c63a19b9388a12fa7c9d14cd5a1
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-21 01:47:28 +1300

    add navigation configuration for admin users

--- config/nav-config.ts
+++ config/nav-config.ts
@@ -5,3 +5,10 @@ export const NAV_CONFIG = [
     { title: en.navigation.account.label, href: join('user', 'account') },
     { title: en.navigation.order.label, href: join('user', 'order') },
 ]
+
+export const NAV_CONFIG_ADMIN = [
+    { title: en.overview.label, href: join('admin', 'overview') },
+    { title: en.product.label, href: join('admin', 'product') },
+    { title: en.order.label, href: join('admin', 'order') },
+    { title: en.user.label, href: join('admin', 'user') },
+]
