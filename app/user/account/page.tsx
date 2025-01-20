14fb9fcfb31eb892143db13a65df4291ff686eed
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-21 00:46:34 +1300

    ✨ [feat]: enhance UserAccountPage with session management and AccountForm integration

--- app/user/account/page.tsx
+++ app/user/account/page.tsx
@@ -1,7 +1,21 @@
-import { Fragment } from 'react'
+import { Metadata } from 'next'
+import { auth } from 'auth'
+import { en } from 'public/locale'
+import { SessionProvider} from 'next-auth/react'
+import AccountForm from './account-form'
 
-const UserAccountPage = () => {
-  return <Fragment>{'User Account'}</Fragment>
+export const metadata: Metadata = { title: 'Account' }
+
+const UserAccountPage = async () => {
+  const session = await auth()
+  return <SessionProvider session={session}>
+    <div className="max-w-md mx-auto space-y-4">
+      <h2 className="h2-bold">
+      {en.navigation.account.label}
+      </h2>
+      <AccountForm />
+    </div>
+  </SessionProvider>
 }
 
 export default UserAccountPage
