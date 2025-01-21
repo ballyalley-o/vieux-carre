c89eccbe07bc4bfe3a2dab123be26008a6d940fd
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-21 23:48:08 +1300

    add design tokens for logo dimensions in layout configuration

--- config/layout.ts
+++ config/layout.ts
@@ -1,7 +1,11 @@
+/**
+ * DESIGN TOKENS
+ */
 export const LAYOUT = {}
 
 export const LOGO = {
     NAV_ADMIN_H: 30,
-    NAV_ADMIN_W: 30
-
+    NAV_ADMIN_W: 30,
+    NAV_H      : 24,
+    NAV_W      : 24
 }
