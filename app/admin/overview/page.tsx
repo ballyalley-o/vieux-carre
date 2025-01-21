cdd64ff2b914641216dfcbd1743d84f1052319ee
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-21 23:49:47 +1300

    [feat-init]: add admin overview page with order summary retrieval and authorization check

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,7 +1,30 @@
-import { Fragment } from "react";
+import { Metadata } from 'next'
+import { en } from 'public/locale'
+import { auth } from 'auth'
+import { getOrderSummary, KEY } from 'lib'
+import { Card, CardHeader, CardTitle } from 'component/ui'
 
-const AdminOverviewPage = () => {
-    return ( <Fragment>{'Admin overview page'}</Fragment> );
+export const metadata: Metadata = { title: 'Admin Overview' }
+
+const AdminOverviewPage = async () => {
+    const session = await auth()
+    const isAdmin = session?.user?.role === KEY.ADMIN
+    if (!isAdmin) throw new Error(en.error.user_not_authorized)
+
+    const summary = await getOrderSummary()
+    console.log(summary)
+  return <div className={'space-y-2'}>
+    <h2 className="h2-bold">{en.overview.label}</h2>
+    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <Card>
+            <CardHeader className={'flex flex-row items-center justify-between space-y-0 pb-2'}>
+                <CardTitle className={'text-sm font-medium'}>
+                    {en.total_revenue.label}
+                </CardTitle>
+            </CardHeader>
+        </Card>
+    </div>
+    {JSON.stringify(summary)}</div>
 }
 
-export default AdminOverviewPage;
+export default AdminOverviewPage
