bf65013c61ac2674434da25b73526684a0273ed8
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-24 23:02:46 +1300

    🦄 [refax]: add getAllProducts function for paginated product retrieval and update import path for GLOBAL

--- lib/action/product.action.ts
+++ lib/action/product.action.ts
@@ -1,7 +1,9 @@
 'use server'
 import { prisma } from 'db/prisma'
-import { GLOBAL } from 'config'
+import { GLOBAL } from 'vieux-carre'
 import { convertToPlainObject } from 'lib'
+
+
 
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({ take: GLOBAL.LATEST_PRODUCT_QUANTITY, orderBy: { createdAt: 'desc' } })
@@ -10,4 +12,13 @@ export async function getLatestProducts() {
 
 export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({ where: { slug } })
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function getAllProducts({ query, limit = GLOBAL.PAGE_SIZE, page, category }: AppProductsAction<number>) {
+  const data  = await prisma.product.findMany({ skip: (page - 1) * limit, take: limit })
+  const count = await prisma.product.count()
+
+  const summary = { data, totalPages: Math.ceil(count / limit)}
+  return summary
 }
