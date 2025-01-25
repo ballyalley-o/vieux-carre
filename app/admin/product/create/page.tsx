d36d0ebdf5ec404cf0aeadd3bfdbe7fb6840c34d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-25 16:23:29 +1300

    ✨ [feat]: add AdminCreateProductPage for product creation

---
+++ app/admin/product/create/page.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from 'react'
+import { Metadata } from 'next'
+import { en } from 'public/locale'
+import { ProductForm } from 'component/admin'
+
+export const metadata: Metadata = { title: 'Create Product | Admin' }
+
+const AdminCreateProductPage = () => {
+  return (
+    <Fragment>
+      <h2 className="h2-bold">{en.create_product.label}</h2>
+      <div className="my-8">
+        <ProductForm type={'create'}/>
+      </div>
+    </Fragment>
+  )
+}
+
+export default AdminCreateProductPage
