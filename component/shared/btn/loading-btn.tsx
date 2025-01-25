dbcb36dea5b52f066a8c667bae734b8f5a2603f9
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-25 22:19:46 +1300

    [refax]: add className prop to LoadingBtn for customizable styling

--- component/shared/btn/loading-btn.tsx
+++ component/shared/btn/loading-btn.tsx
@@ -3,6 +3,7 @@ import { ArrowRight } from 'lucide-react'
 import { Button } from 'component/ui'
 import { EllipsisLoader } from 'component/shared/loader'
 import { KEY } from 'lib'
+import { cn } from 'lib/util'
 
 interface LoadingBtnProps {
   isPending : boolean
@@ -10,6 +11,7 @@ interface LoadingBtnProps {
   icon     ?: JSX.Element
   type     ?: ButtonType
   disabled ?: boolean
+  className?: string
 }
 
 const LoadingBtn: FC<LoadingBtnProps> = ({
@@ -17,11 +19,12 @@ const LoadingBtn: FC<LoadingBtnProps> = ({
   icon = <ArrowRight className={'default-size_icon'} />,
   label,
   type = KEY.SUBMIT as ButtonType,
-  disabled = false
+  disabled = false,
+  className
 }) => {
   return (
     <div className="flex gap-2">
-      <Button type={type} disabled={isPending || disabled} className="w-full mt-5">
+      <Button type={type} disabled={isPending || disabled} className={cn("mt-5", className)}>
         {isPending ? (
           <EllipsisLoader />
         ) : (
