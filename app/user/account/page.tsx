c5e032b56c80d6d9c83d905a212018a1e88acf5b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-26 17:23:42 +1300

    ✨ [feat-wire-up]: fetch user data and pass to AccountForm in UserAccountPage for improved user experience

--- app/user/account/page.tsx
+++ app/user/account/page.tsx
@@ -1,19 +1,25 @@
-import { Metadata } from 'next'
-import { auth } from 'auth'
-import { en } from 'public/locale'
 import { SessionProvider} from 'next-auth/react'
+import { Metadata } from 'next'
+import { en } from 'public/locale'
+import { auth } from 'auth'
+import { getUserById } from 'lib'
 import AccountForm from './account-form'
 
 export const metadata: Metadata = { title: 'Account' }
 
 const UserAccountPage = async () => {
   const session = await auth()
+  const userId = session?.user?.id
+
+  if (!userId) throw new Error(en.error.user_not_found)
+  const user = await getUserById(userId)
+
   return <SessionProvider session={session}>
     <div className="max-w-md mx-auto space-y-4">
       <h2 className="h2-bold">
       {en.navigation.account.label}
       </h2>
-      <AccountForm />
+      <AccountForm user={user} />
     </div>
   </SessionProvider>
 }
