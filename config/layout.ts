a7b6ff19a779d5d2bd2f5c3fe08f496fa5282708
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-26 17:35:23 +1300

    [design-tokens]: add HEADER_LOGO dimensions to layout configuration

--- config/layout.ts
+++ config/layout.ts
@@ -4,8 +4,10 @@
 export const LAYOUT = {}
 
 export const LOGO = {
-    NAV_ADMIN_H: 30,
-    NAV_ADMIN_W: 30,
-    NAV_H      : 24,
-    NAV_W      : 24
+    NAV_ADMIN_H  : 30,
+    NAV_ADMIN_W  : 30,
+    NAV_H        : 24,
+    NAV_W        : 24,
+    HEADER_LOGO_H: 24,
+    HEADER_LOGO_W: 24
 }
