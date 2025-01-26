9acca0b72ea22584b3ab7a69167fa783f1621f4b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-26 17:22:24 +1300

    ✨ [refax]: replace BackBtn with FormBackBtn for improved navigation in AdminCreateProductPage

--- app/admin/product/create/page.tsx
+++ app/admin/product/create/page.tsx
@@ -1,15 +1,16 @@
 import { Fragment } from 'react'
 import { Metadata } from 'next'
 import { en } from 'public/locale'
-import { BackBtn } from 'component/shared'
 import { ProductForm } from 'component/admin'
+import { FormBackBtn } from 'component/shared/btn'
+import { PATH_DIR } from 'config'
 
 export const metadata: Metadata = { title: 'Create Product | Admin' }
 
 const AdminCreateProductPage = () => {
   return (
     <Fragment>
-      <BackBtn/>
+      <FormBackBtn href={PATH_DIR.ADMIN.PRODUCT} withLink />
       <h2 className="h2-bold">{en.create_product.label}</h2>
       <div className="my-8">
         <ProductForm type={'create'} />
