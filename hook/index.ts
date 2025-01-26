cfabc1d54d6704ea12d20c6d5b83d72039db691b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-26 17:33:18 +1300

    ✨ [feat-navigation]: add usePreventNavigation hook to prevent navigation on dirty forms

--- hook/index.ts
+++ hook/index.ts
@@ -1,2 +1,3 @@
 export * from './use-toast'
 export * from './use-app-colors'
+export { default as usePreventNavigation } from './use-prevent-navigation'
---
+++ hook/use-prevent-navigation.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+
+function usePreventNavigation(isDirty: boolean) {
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      if (isDirty) {
+        e.preventDefault()
+        e.returnValue = ''
+      }
+    }
+
+    window.addEventListener('beforeunload', handleBeforeUnload)
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    }
+  }, [isDirty])
+}
+
+export default usePreventNavigation
