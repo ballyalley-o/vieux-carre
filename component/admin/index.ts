03699fef44bae882bdbdce1131837ef90276f21e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-29 00:31:17 +1300

    [feat]: add AdminSearch component for enhanced admin search functionality

--- component/admin/index.ts
+++ component/admin/index.ts
@@ -1,2 +1,5 @@
+// ⎿ form
 export { default as ProductForm } from './form/product-form'
 export { default as UserAccountForm } from './form/user-account-form'
+// ⎿ search
+export { default as AdminSearch } from './search/admin-search'
