9a49c833049e688563da09f2fd64ac9ca123ca65
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-29 04:10:01 +1300

    [interface]: enhance AppOrdersAction and AppUser interfaces with query property for improved data handling

--- interface/global.d.ts
+++ interface/global.d.ts
@@ -100,7 +100,8 @@ declare global {
 
   export interface AppPage<T> { page: T }
   export interface AppPageAction<T> extends AppPage<T> { limit?: number }
-  export interface AppOrdersAction<T> extends AppPage<T> { limit?: number }
+  export interface AppOrdersAction<T> extends AppPage<T> { limit?: number, query: string }
+  export interface AppUser<T> extends AppPage<T> { limit?: number, query: string }
   export interface AppProductsPage<T> extends AppPage<T> { query: string, category: string }
   export interface AppProductsAction<T> extends AppPage<T> { query: string, category: string, limit?: number }
 
