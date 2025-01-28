38ff27bc43e4ce5d74c59b0044993eb30c7a9c70
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-29 00:31:35 +1300

    [feat-enhance]: enhance AdminProductsPage with query display and improved no data message

--- app/admin/product/page.tsx
+++ app/admin/product/page.tsx
@@ -3,9 +3,9 @@ import { en } from 'public/locale'
 import { Metadata } from 'next'
 import Link from 'next/link'
 import { formatCurrency, formatId, getAllProducts, deleteProduct, generateTitle } from 'lib'
-import { FilePenLine, ListMinus, SquareArrowOutUpRight, Ellipsis } from 'lucide-react'
-import { Button, Table } from 'component/ui'
-import { TblHead, TblBody, Pagination, DDMenu, DeleteDialg, TooltpGoBadge, Tooltp } from 'component/shared'
+import { FilePenLine, ListMinus, SquareArrowOutUpRight, Ellipsis, XIcon } from 'lucide-react'
+import { Button, Table, Badge } from 'component/ui'
+import { TblHead, TblBody, Pagination, DDMenu, DeleteDialg, TooltpGoBadge, Tooltp, LinkBtn } from 'component/shared'
 import { PATH_DIR } from 'config'
 
 export const metadata: Metadata = { title: generateTitle(en.product.products.label, 'Admin') }
@@ -16,7 +16,6 @@ interface AdminProductsPageProps {
 const AdminProductsPage: FC<AdminProductsPageProps> = async ({ searchParams }) => {
     const { page, query, category } = await searchParams
     const products                  = await getAllProducts({ query, page: Number(page) || 1, category })
-
 
     const MENU_ITEMS = (item: Product) =>  {
        return ([
@@ -54,7 +53,14 @@ const AdminProductsPage: FC<AdminProductsPageProps> = async ({ searchParams }) =
     return (
     <div className={'space-y-2'}>
         <div className={'flex-between'}>
-            <h1 className={'h2-bold'}>{en.product.products.label}</h1>
+            <div className="space-y-4 gap-3">
+                <h1 className={'h2-bold'}>{en.product.products.label}</h1>
+                {query && (
+                    <div className={'flex items-center align-center space-x-2'}>
+                        <p className={'text-sm'}> &nbsp;{en.filtered_by.label}:</p> <span><div className={'align-center'}> <Badge variant={'secondary'}><i>{query}</i></Badge> <LinkBtn href={PATH_DIR.ADMIN.PRODUCT} variant={'ghost'} size={'sm'} className={'py-0 px-2'}><XIcon size={15}/></LinkBtn></div></span>
+                    </div>
+                )}
+            </div>
             <Button asChild variant={'outline'}>
                 <Link href={PATH_DIR.ADMIN.PRODUCT_CREATE}>{en.create_product.label}</Link>
             </Button>
@@ -63,7 +69,7 @@ const AdminProductsPage: FC<AdminProductsPageProps> = async ({ searchParams }) =
             <TblHead cells={HEAD.cells} />
             <TblBody cells={BODY} items={products.data as unknown as Product[]}/>
         </Table>
-        {products.totalPages === 0 && (<div className={'flex justify-center'}> <p className={'text-center text-lg'}>{en.no_data.label}</p> </div>)}
+        {products.totalPages === 0 && (<div className={'flex justify-center space-y-6'}> <p className={'text-center text-2xl text-muted-foreground'}>{en.zero_results.label}</p></div>)}
         {products.totalPages > 1 && (
             <div className="mt-5 flex justify-end">
                 <Pagination page={Number(page) || 1} totalPages={products.totalPages}/>
