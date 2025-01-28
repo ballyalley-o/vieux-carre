b9f7a16c1668005d284491e89c74c5096780d5cc
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-29 04:11:29 +1300

    [feat]: add PageTitle component for displaying titles with optional query filtering

--- component/admin/index.ts
+++ component/admin/index.ts
@@ -3,3 +3,5 @@ export { default as ProductForm } from './form/product-form'
 export { default as UserAccountForm } from './form/user-account-form'
 // ⎿ search
 export { default as AdminSearch } from './search/admin-search'
+// ⎿ title
+export { default as PageTitle } from './title/page-title'
