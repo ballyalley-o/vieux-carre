7cc8d404c01144a680909f7d2f1f59e5d5e46b1e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-30 03:14:22 +1300

    [feat-propagation]: add space after CategoryDrwr component for improved readability

--- component/shared/header/header.tsx
+++ component/shared/header/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <CategoryDrwr/>
+          <CategoryDrwr />
           <ProtectedNavLink href={PATH_DIR.ROOT} className={'flex-start ml-4'}>
             <Image src={ASSET_DIR.LOGO_RED} alt="logo" width={LOGO.HEADER_LOGO_W} height={LOGO.HEADER_LOGO_H} priority />
             <span className="hidden lg:block ml-3">{GLOBAL.APP_NAME}</span>
