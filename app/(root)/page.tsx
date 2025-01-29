48d5800182cfb5707c1e4b55d4fcc4c2bea07ab1
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-30 03:12:19 +1300

    [feat-propagation]: add ProductBtn component to Homepage for enhanced user interaction

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { getLatestProducts, getAllFeaturedProducts } from 'lib'
-import { ProductList, ProductCarousel } from 'component'
+import { ProductList, ProductCarousel, ProductBtn } from 'component/shared'
 import { GLOBAL } from 'config'
 
 const Homepage = async () => {
@@ -10,6 +10,7 @@ const Homepage = async () => {
     <Fragment>
      {featuredProducts.length > 0  && <ProductCarousel products={featuredProducts} />}
       <ProductList data={latestProducts} title={'Newest Arrivals'} limit={GLOBAL.LATEST_PRODUCT_QUANTITY} />
+      <ProductBtn />
       <h1 className="text-8xl font-bold">{'Shop.Go.'}</h1>
       <h6 className="mt-4">This is the homepage.</h6>
     </Fragment>
