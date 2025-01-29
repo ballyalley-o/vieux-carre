1d3c596b5c37b0e6d867c2173d6eedd969917d08
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-29 18:27:06 +1300

    [feat-propagation]: integrate ProductCarousel for featured products on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import { Fragment } from 'react'
-import { getLatestProducts } from 'lib'
-import { ProductList } from 'component'
+import { getLatestProducts, getAllFeaturedProducts } from 'lib'
+import { ProductList, ProductCarousel } from 'component'
 import { GLOBAL } from 'config'
+
 const Homepage = async () => {
-  const latestProducts = await getLatestProducts()
+  const latestProducts   = await getLatestProducts()
+  const featuredProducts = await getAllFeaturedProducts()
   return (
     <Fragment>
+     {featuredProducts.length > 0  && <ProductCarousel products={featuredProducts} />}
       <ProductList data={latestProducts} title={'Newest Arrivals'} limit={GLOBAL.LATEST_PRODUCT_QUANTITY} />
       <h1 className="text-8xl font-bold">{'Shop.Go.'}</h1>
       <h6 className="mt-4">This is the homepage.</h6>
