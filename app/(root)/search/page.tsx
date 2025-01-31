e9651117a986180cca3d1e0a0388dabf12b23424
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-01-31 22:34:23 +1300

    [feat-enhance]: enhance SearchPage with default query handling and clear filters button

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,16 +1,18 @@
 import { FC, Fragment } from 'react'
 import { en } from 'public/locale'
 import { getAllProducts, getAllCategories, PRICE, RATING } from 'lib'
-import { NoResult, ProductCard } from 'component/shared'
+import { NoResult, ProductCard, LinkBtn } from 'component/shared'
 import { PATH_DIR } from 'config'
 import FilterList from './filter-list'
 import FilterTitle from './filter-title'
+
+const DEFAULT_QUERY = 'all'
 
 interface SearchPageProps {
   searchParams: Promise<AppSearchPage<string>>
 }
 const SearchPage: FC<SearchPageProps> = async ({ searchParams }) => {
-  const { query = 'all', category = 'all', price = 'all', rating = 'all', sort = 'newest', page = '1' } = await searchParams
+  const { query = DEFAULT_QUERY, category = DEFAULT_QUERY, price = DEFAULT_QUERY, rating = DEFAULT_QUERY, sort = 'newest', page = '1' } = await searchParams
 
   const getFilterUrl = ({ _category , _price, _rating, _sort, _page }:{ _category?: string, _price?: string, _rating?: string, _sort?: string, _page?: string})  => {
     const params = { query, category, price, rating, sort, page }
@@ -58,8 +60,11 @@ const SearchPage: FC<SearchPageProps> = async ({ searchParams }) => {
         <div className="md:col-span-4 space-y-4">
           <div className="flex-between flex-col md:flex-row my-4">
             <div className="flex items-center">
-              {/* {query !== 'all' && query !== '' &&} */}
-              <FilterTitle href={PATH_DIR.SEARCH} filter={query} filterTypeLabel={category !== 'all' && (`${en.category.label}:  ${category}`) || ''}  />
+              {query !== DEFAULT_QUERY && query !== '' && (<FilterTitle href={PATH_DIR.SEARCH} filter={query} filterTypeLabel={en.query.label || ''}  /> )}
+              {category !== DEFAULT_QUERY && category !== '' && (<FilterTitle href={getFilterUrl({ _category: DEFAULT_QUERY })} filter={category} filterTypeLabel={en.category.label || ''}  /> )}
+              {price !== DEFAULT_QUERY && (<FilterTitle href={getFilterUrl({ _price: DEFAULT_QUERY })} filter={price} filterTypeLabel={en.price.label || ''}  /> )}
+              {rating !== DEFAULT_QUERY && (<FilterTitle href={getFilterUrl({ _rating: DEFAULT_QUERY })} filter={`${rating} stars +`} filterTypeLabel={en.rating.label || ''}  /> )}
+              {(category !== DEFAULT_QUERY || price !== DEFAULT_QUERY || rating !== DEFAULT_QUERY) && <LinkBtn href={PATH_DIR.SEARCH} variant={'ghost'} size={'sm'} className={'py-0 px-2 text-red-500'}>{en.clear.label}</LinkBtn>}
             </div>
           </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
