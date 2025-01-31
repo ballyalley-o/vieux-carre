9eafacd21ed67158efa509644efb1d4733c8e6b2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-01 01:49:37 +1300

    [bugfix]: replace constant KEY with hardcoded sessionBagId in authConfig for cookie handling

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,6 @@
 import type { NextAuthConfig } from "next-auth"
 import { NextResponse } from "next/server"
 import { PROTECTED_ROUTES } from 'config'
-import { KEY } from 'lib/constant'
 
 export const authConfig = {
     providers: [],
@@ -10,11 +9,11 @@ export const authConfig = {
         authorized({ request, auth }: any) {
                 const { pathname } = request.nextUrl
                 if (!auth && PROTECTED_ROUTES.some((p) => p.test(pathname))) return false
-                if (!request.cookies.get(KEY.SESSION_BAG_ID)) {
+                if (!request.cookies.get('sessionBagId')) {
                   const sessionBagId = crypto.randomUUID()
                   const newRequestHeaders = new Headers(request.headers)
                   const response = NextResponse.next({ request: { headers: newRequestHeaders } })
-                  response.cookies.set(KEY.SESSION_BAG_ID, sessionBagId)
+                  response.cookies.set('sessionBagId', sessionBagId)
                   return response
                 }
                 return true
