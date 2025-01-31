ecabb8be2ea10cf9e58a05bb407378daf0e3d2ef
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-01 00:17:46 +1300

    [bugfix-middleware]: implement new authentication configuration and move authorization logic to auth.config.ts

--- auth.ts
+++ auth.ts
@@ -87,19 +87,19 @@ export const config = {
       }
       return token
     },
-    authorized({ request, auth }: any) {
-      const { pathname } = request.nextUrl
-      if (!auth && PROTECTED_ROUTES.some(p => p.test(pathname))) return false
-      if (!request.cookies.get(KEY.SESSION_BAG_ID)) {
-        const sessionBagId = crypto.randomUUID()
-        const newRequestHeaders = new Headers(request.headers)
-        const response = NextResponse.next({ request: { headers: newRequestHeaders } })
-        response.cookies.set(KEY.SESSION_BAG_ID, sessionBagId)
-        return response
-      } else {
-        return true
-      }
-    }
+    // authorized({ request, auth }: any) {
+    //   const { pathname } = request.nextUrl
+    //   if (!auth && PROTECTED_ROUTES.some(p => p.test(pathname))) return false
+    //   if (!request.cookies.get(KEY.SESSION_BAG_ID)) {
+    //     const sessionBagId = crypto.randomUUID()
+    //     const newRequestHeaders = new Headers(request.headers)
+    //     const response = NextResponse.next({ request: { headers: newRequestHeaders } })
+    //     response.cookies.set(KEY.SESSION_BAG_ID, sessionBagId)
+    //     return response
+    //   } else {
+    //     return true
+    //   }
+    // }
   }
 } satisfies NextAuthConfig
 
