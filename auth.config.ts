13cbecc98b4e8dcd3f6c75cb1644cbddd3213877
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-01 00:41:02 +1300

    [formatting]: improve formatting and readability of authorization callback in auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -7,18 +7,18 @@ import { KEY } from 'lib/constant'
 export const authConfig = {
     providers: [],
     callbacks: {
-    authorized({ request, auth }: any) {
-            const { pathname } = request.nextUrl
-            if (!auth && PROTECTED_ROUTES.some((p) => p.test(pathname))) return false
-            if (!request.cookies.get(KEY.SESSION_BAG_ID)) {
-              const sessionBagId = crypto.randomUUID()
-              const newRequestHeaders = new Headers(request.headers)
-              const response = NextResponse.next({ request: { headers: newRequestHeaders } })
-              response.cookies.set(KEY.SESSION_BAG_ID, sessionBagId)
-              return response
-            } else {
-              return true
+        authorized({ request, auth }: any) {
+                const { pathname } = request.nextUrl
+                if (!auth && PROTECTED_ROUTES.some((p) => p.test(pathname))) return false
+                if (!request.cookies.get(KEY.SESSION_BAG_ID)) {
+                  const sessionBagId = crypto.randomUUID()
+                  const newRequestHeaders = new Headers(request.headers)
+                  const response = NextResponse.next({ request: { headers: newRequestHeaders } })
+                  response.cookies.set(KEY.SESSION_BAG_ID, sessionBagId)
+                  return response
+                } else {
+                  return true
+                }
             }
-        }
     }
 } satisfies NextAuthConfig
