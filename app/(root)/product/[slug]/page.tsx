64291b1c59c2d258ed9382e1f1f81b5dffc24409
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-02 01:33:04 +1300

    [feat]: enhance product view page with customer reviews and rating display

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,17 +1,22 @@
-import { FC } from 'react'
+import { FC, Fragment } from 'react'
+import { en } from 'public/locale'
 import { notFound } from 'next/navigation'
+import { auth } from 'auth'
 import { getProductBySlug, getMyBag } from 'lib'
 import { Button, Card, CardContent, Badge, ProductPrice, ProductImage } from 'component'
-import { AddToBag } from 'component/shared'
+import { AddToBag, ProductRating } from 'component/shared'
+import { ReviewList } from 'component/shared/review'
 
 interface ProductViewPageProps {
   params: Promise<{ slug: string }>
 }
 const ProductViewPage: FC<ProductViewPageProps> = async ({ params }) => {
   const { slug } = await params
-  const product = await getProductBySlug(slug)
+  const product  = await getProductBySlug(slug)
   if (!product) return notFound()
-  const bag = await getMyBag()
+  const session = await auth()
+  const userId  = session?.user?.id
+  const bag     = await getMyBag()
 
   const bagProduct = { productId: product.id, name: product.name, slug: product.slug, price: product.price, qty: 1, image: product.images![0] }
 
@@ -19,6 +24,7 @@ const ProductViewPage: FC<ProductViewPageProps> = async ({ params }) => {
   const renderAddToBagButton = product.stock > 0 ? <AddToBag bag={bag} item={bagProduct} /> : <Button disabled>{'Out of Stock'}</Button>
 
   return (
+    <Fragment>
     <section>
       <div className="grid grid-cols-1 md:grid-cols-5">
         <div className="col-span-2">
@@ -30,9 +36,8 @@ const ProductViewPage: FC<ProductViewPageProps> = async ({ params }) => {
               {product.brand} {product.category}
             </p>
             <h1 className="h3-bold">{product.name}</h1>
-            <p>
-              {product.rating} of {product.numReviews} {'Reviews'}
-            </p>
+            <ProductRating value={Number(product.rating)}/>
+            <p>{product.numReviews} {product.numReviews > 1 ? en.review.reviews.label : en.review.label}</p>
             <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
               <ProductPrice value={Number(product.price)} className="w-24 rounded-sm px-5 py-2" />
             </div>
@@ -60,6 +65,11 @@ const ProductViewPage: FC<ProductViewPageProps> = async ({ params }) => {
         </div>
       </div>
     </section>
+    <section className={'mt-10'}>
+      <h2 className={"h2-bold mb-5"}>{en.customer_review.customer_reviews.label}</h2>
+      <ReviewList userId={userId || ''} productId={product.id} productSlug={product.slug} />
+    </section>
+    </Fragment>
   )
 }
 
