e2bbed530986b1c63eb5701d61715508dc6325d7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-01 16:20:27 +1300

    [feat]: update authorization logic to use inline protected paths for route access control

--- auth.config.ts
+++ auth.config.ts
@@ -1,14 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextAuthConfig } from "next-auth"
 import { NextResponse } from "next/server"
-import { PROTECTED_ROUTES } from 'config'
 
 export const authConfig = {
     providers: [],
     callbacks: {
         authorized({ request, auth }: any) {
+                const protectedPaths = [
+                  /\/shipping/,
+                  /\/payment/,
+                  /\/place-order/,
+                  /\/account/,
+                  /\/user\/(.*)/,
+                  /\/order\/(.*)/,
+                  /\/admin/,
+                ];
                 const { pathname } = request.nextUrl
-                if (!auth && PROTECTED_ROUTES.some((p) => p.test(pathname))) return false
+                if (!auth && protectedPaths.some((p) => p.test(pathname))) return false
                 if (!request.cookies.get('sessionBagId')) {
                   const sessionBagId = crypto.randomUUID()
                   const newRequestHeaders = new Headers(request.headers)
