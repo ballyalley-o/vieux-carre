d1db05de13b01e6988e487d0a60a1a52d1923261
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-03 02:27:19 +1300

    🧹 [housekeeping]: remove unnecessary semicolons in jest.setup.ts and next.config.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv';
-import path from 'path';
+import * as dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config({
   path: path.resolve(process.cwd(), '.env')
