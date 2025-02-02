5073cfed4fc3631db29e2d709e1d9b281acd79ee
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-02 21:40:19 +1300

    [util]: add joinServer utility function for server path concatenation

--- lib/util/join.ts
+++ lib/util/join.ts
@@ -1,3 +1,7 @@
 export const join = (...args: string[]): string => {
   return '/' + args.join('/')
 }
+
+export const joinServer = (server: string, ...args: string[]): string => {
+  return server + '/' + args.join('/')
+}
