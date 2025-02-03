dd8bea440b307328db902e138a4314d7fe5ce7bc
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-04 01:57:11 +1300

    add variant and onClick props to LoadingBtn component

--- component/shared/btn/loading-btn.tsx
+++ component/shared/btn/loading-btn.tsx
@@ -8,23 +8,27 @@ import { cn } from 'lib/util'
 interface LoadingBtnProps {
   isPending : boolean
   label     : string
+  variant  ?: ButtonVariant
   icon     ?: JSX.Element
   type     ?: ButtonType
   disabled ?: boolean
   className?: string
+  onClick  ?: () => void
 }
 
 const LoadingBtn: FC<LoadingBtnProps> = ({
   isPending,
   icon = <ArrowRight className={'default-size_icon'} />,
   label,
-  type = KEY.SUBMIT as ButtonType,
+  type     = KEY.SUBMIT as ButtonType,
   disabled = false,
+  variant  = 'default',
+  onClick,
   className
 }) => {
   return (
     <div className="flex gap-2">
-      <Button type={type} disabled={isPending || disabled} className={cn("mt-5", className)}>
+      <Button type={type} variant={variant} disabled={isPending || disabled} className={cn("mt-5", className)} onClick={onClick}>
         {isPending ? (
           <EllipsisLoader />
         ) : (
