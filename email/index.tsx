bd9b14da7b167aa04803b101560c182ddd27a552
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-04 01:57:25 +1300

    [cleanup]: remove dotenv configuration from email module

--- email/index.tsx
+++ email/index.tsx
@@ -2,9 +2,6 @@ import { Resend } from 'resend'
 import { GLOBAL } from '../config/global'
 import PurchaseReceiptEmail from './purchase-receipt'
 
-import * as dotenv from 'dotenv'
-dotenv.config()
-
 const resend = new Resend(GLOBAL.RESEND.RESEND_API_KEY as string)
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
