808f9ec8c96004f65d2a3c080fbc20bccaf95082
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-05 00:06:08 +1300

    [cleanup]: add className prop to TblBody for customizable cell styling

--- component/shared/tbl/tbl-body.tsx
+++ component/shared/tbl/tbl-body.tsx
@@ -1,21 +1,19 @@
 import { TableBody, TableRow, TableCell } from 'component/ui'
+import { cn } from 'lib'
 
 export interface TblBodyProps<T> {
-  items: T[]
-  cells: (item: T) => TblCells<number>
+  items     : T[]
+  cells     : (item: T) => TblCells<number>
+  className?: string
 }
 
-// interface TblBodyProps<T extends TblCell[]> {
-//   rows: Array<{ [K in keyof T]: T[K]['value'] }>
-// }
-
-const TblBody = <T,>({ cells, items }: TblBodyProps<T>) => {
+const TblBody = <T,>({ cells, items, className }: TblBodyProps<T>) => {
   return (
     <TableBody>
       {items.map((item, rowIndex) => (
         <TableRow key={rowIndex}>
           {cells(item).cells.map((cell) => (
-            <TableCell key={cell.id} className={`text-${cell.align}`}>
+            <TableCell key={cell.id} className={cn(`text-${cell.align}`, className && className)}>
               {cell.value}
             </TableCell>
           ))}
