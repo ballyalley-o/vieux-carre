010f0cc09867de964861e121ebfb165be163d37f
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-05 22:40:49 +1300

    [ui-update]: enhance product list layout for improved responsiveness and add toolbar label to localization

--- component/shared/product/product-list.tsx
+++ component/shared/product/product-list.tsx
@@ -14,10 +14,12 @@ const ProductList: FC<ProductListProps> = ({ data, title, limit }) => {
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: Product, index: number) => (
-            <ProductCard key={index} product={product} />
-          ))}
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {limitedData.map((product: Product, index: number) => (
+              <ProductCard key={index} product={product} />
+            ))}
+          </div>
         </div>
       ) : (
         <div>
