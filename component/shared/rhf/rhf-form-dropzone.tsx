3cab53d59f5fe7f8042d8cd1ee54b56847d2f979
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-11 01:19:27 +1000

    [feat]: add delete functionality for uploaded images in RHFFormDropzone

--- component/shared/rhf/rhf-form-dropzone.tsx
+++ component/shared/rhf/rhf-form-dropzone.tsx
@@ -5,6 +5,8 @@ import { en } from 'public/locale'
 import { Control, Path, PathValue, UseFormReturn } from 'react-hook-form'
 import Image from 'next/image'
 import { useToast } from 'hook'
+import { deleteProductImage } from 'lib/action'
+import { X } from 'lucide-react'
 import { FormField, FormLabel, FormMessage, FormItem, FormControl } from 'component/ui/form'
 import { Card, CardContent } from 'component/ui/card'
 import { IMAGE } from 'config/layout'
@@ -26,7 +28,19 @@ const RHFFormDropzone = <TSchema extends ZodSchema>({ control, name, images, for
   const handleUploadComplete = (res: { url: string }[]) => {
     const currentImages = form.getValues(name) as string[]
     form.setValue(name, [...currentImages, res[0].url] as PathValue<z.infer<TSchema>, Path<z.infer<TSchema>>>)
-  };
+  }
+
+  const handleDelete = async (index: number) => {
+    const currentImages = form.getValues(name) as string[]
+    const result = await deleteProductImage(currentImages, index)
+    if (result?.success) {
+      const updatedImages = currentImages.filter((_, _i) => _i != index)
+      form.setValue(name, updatedImages as PathValue<z.infer<TSchema>, Path<z.infer<TSchema>>>)
+      toast({ description: 'File deleted' })
+    } else {
+      toast({ variant: 'destructive', description: 'Failed to delete file from server' })
+    }
+  }
   return (
     <FormField
         control={control}
@@ -38,7 +52,10 @@ const RHFFormDropzone = <TSchema extends ZodSchema>({ control, name, images, for
                     <CardContent className={'space-y-2 mt-2 min-h-48'}>
                         <div className="flex-start space-x-2">
                             {images.map((image, index) => (
-                                <Image key={index} src={image} height={IMAGE.UPLOAD_THUMBNAIL_H} width={IMAGE.UPLOAD_THUMBNAIL_W} alt={'product-name'} className={'w-20 h-20 object-cover rounded-sm'}  />
+                                <div key={index} className={'relative'}>
+                                  <X size={20} color={'red'} className={'absolute top-0 right-0 cursor-pointer'} onClick={() => handleDelete(index)}/>
+                                  <Image key={index} src={image} height={IMAGE.UPLOAD_THUMBNAIL_H} width={IMAGE.UPLOAD_THUMBNAIL_W} alt={'product-name'} className={'w-20 h-20 object-cover rounded-sm'}  />
+                                </div>
                             ))}
                             <FormControl>
                               <UploadButton endpoint={'imageUploader'} onClientUploadComplete={handleUploadComplete} onUploadError={(error: Error) => { toast({ variant: 'destructive', description: error.message })}} className={'border-none'} appearance={{  button: 'px-2 bg-transparent', container: 'm-auto' }} />
