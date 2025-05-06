7e9f1db32d71435e861fb71a7a5d0bf435202ec2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-06 23:31:20 +1000

    [fix]: update S3 client credentials handling in upload route

--- app/admin/upload/route.ts
+++ app/admin/upload/route.ts
@@ -1,13 +1,13 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { GLOBAL } from 'vieux-carre'
 import { NextRequest, NextResponse } from 'next/server'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { v4 as uuidv4 } from 'uuid'
 
 const s3 = new S3Client({
-  region: GLOBAL.AWS.AWS_REGION,
+  region: GLOBAL.AWS.AWS_REGION!,
   credentials: {
-    accessKeyId    : GLOBAL.AWS.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: GLOBAL.AWS.AWS_SECRET_ACCESS_KEY!
+    accessKeyId    : GLOBAL.AWS.AWS_ACCESS_KEY_ID,
+    secretAccessKey: GLOBAL.AWS.AWS_SECRET_ACCESS_KEY
   }
 })
 
