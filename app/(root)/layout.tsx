5a311d797b37411f2095f3e70ca46d7ffcd49ee1
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-08 18:27:05 +1000

    [Enhance]: Scale the featured products to Hero Fixes #43
    [feature]: fetch and display featured products in the RootLayout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from 'react'
+import { getAllFeaturedProducts } from 'lib'
 import { Header, Footer } from 'component/shared'
 
-export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+export default async function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+  const featuredProducts = await getAllFeaturedProducts()
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      <main className="flex-1 wrapper">{children}</main>
+    <div className={'flex h-screen flex-col'}>
+      <Header products={featuredProducts} />
+      <main className={'flex-1 wrapper'}>{children}</main>
       <Footer />
     </div>
   )
