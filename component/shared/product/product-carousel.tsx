d68a38c46cc759b55a1a75ca6fb9f7ef9f30be25
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-08 18:29:29 +1000

    [refactor]: update ProductCarousel to conditionally render based on pathname

--- component/shared/product/product-carousel.tsx
+++ component/shared/product/product-carousel.tsx
@@ -1,36 +1,37 @@
-'use client'
+"use client"
 
-import { FC } from 'react'
-import Autoplay from 'embla-carousel-autoplay'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from 'component/ui'
+import { usePathname } from 'next/navigation'
+import Autoplay from 'embla-carousel-autoplay'
+import { Carousel, CarouselContent, CarouselItem } from 'component/ui'
 import { PATH_DIR } from 'config'
 
 interface ProductCarouselProps {
     products: Product[]
 }
 
-const ProductCarousel: FC<ProductCarouselProps> = ({ products }) => {
+const ProductCarousel: React.FC<ProductCarouselProps> = ({ products }) => {
+  const pathname = usePathname()
+
   const autoplayConfig = { delay: 10000, stopOnInteraction: true, stopOnMouseEnter: true }
   const carouselConfig = { loop: true }
 
+  if (pathname !== '/') return null
   return (
-  <Carousel className={'w-full mb-12'} opts={carouselConfig} plugins={[Autoplay(autoplayConfig)]}>
-    <CarouselContent>
+    <Carousel className={'w-full mb-12'} opts={carouselConfig} plugins={[Autoplay(autoplayConfig)]}>
+      <CarouselContent>
         {products.map((product, index) => (
-           <CarouselItem key={index}>
-                <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
-                    <div className="relative mx-auto">
-                        <Image src={product.banner!} alt={product.name} height={0} width={0} sizes={'100vw'} className={'w-full h-auto'}/>
-                    </div>
-                </Link>
-           </CarouselItem>
+          <CarouselItem key={index}>
+            <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
+              <div className="relative mx-auto">
+                <Image src={product.banner!} alt={product.name} height={0} width={0} sizes={'100vw'} className={'w-full h-auto'} />
+              </div>
+            </Link>
+          </CarouselItem>
         ))}
-    </CarouselContent>
-    <CarouselPrevious className={'border-none rounded-sm shadow-none'} />
-    <CarouselNext className={'border-none rounded-sm shadow-none'} />
-  </Carousel>
+      </CarouselContent>
+    </Carousel>
   )
 }
 
