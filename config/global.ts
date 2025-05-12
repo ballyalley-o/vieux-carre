be26652349b001f67113959dc8be234309859576
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-12 23:29:51 +1000

    [refactor] added  public url

--- config/global.ts
+++ config/global.ts
@@ -50,6 +50,7 @@ export const GLOBAL = {
                             AWS_ACCESS_KEY_ID    : process.env.AWS_ACCESS_KEY_ID || "",
                             AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || "",
                             AWS_REGION           : process.env.AWS_REGION || "",
-                            S3_BUCKET_NAME       : process.env.S3_BUCKET_NAME || ""
+                            S3_BUCKET_NAME       : process.env.S3_BUCKET_NAME || "",
+                            PUBLIC_URL           : `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/`
                           }
 }
