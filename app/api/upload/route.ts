eda45021be61b825a744ded66ac1316366020401
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-12 23:27:48 +1000

    [fix & refactor]: changesin

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,25 +1,35 @@
 import { GLOBAL } from 'vieux-carre'
+import { en } from 'public/locale'
+import { NextRequest, NextResponse } from 'next/server'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { v4 as uuidv4 } from 'uuid'
 
 const s3 = new S3Client({
-  region: GLOBAL.AWS.AWS_REGION!,
+  region: GLOBAL.AWS.AWS_REGION,
   credentials: {
-    accessKeyId    : GLOBAL.AWS.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: GLOBAL.AWS.AWS_SECRET_ACCESS_KEY!
+    accessKeyId    : GLOBAL.AWS.AWS_ACCESS_KEY_ID,
+    secretAccessKey: GLOBAL.AWS.AWS_SECRET_ACCESS_KEY
   }
 })
 
-export async function POST(req: Request) {
-  const { filename, fileType } = await req.json()
+export async function POST(req: NextRequest) {
+  const formData = await req.formData()
+  const file     = formData.get('file') as File
 
-  const command = new PutObjectCommand({
-    Bucket      : GLOBAL.AWS.S3_BUCKET_NAME!,
-    Key         : filename,
-    ContentType : fileType
-  })
+  if (!file) return NextResponse.json({ error: en.error.file_not_found }, { status: 400 })
 
-  const url = await getSignedUrl(s3, command, { expiresIn: 60 })
+  const fileBuffer = Buffer.from(await file.arrayBuffer())
+  const fileKey = `upload/${uuidv4()}-${file.name}`
 
-  return Response.json({ url })
+  await s3.send(
+    new PutObjectCommand({
+      Bucket     : GLOBAL.AWS.S3_BUCKET_NAME,
+      Key        : fileKey,
+      Body       : fileBuffer,
+      ContentType: file.type
+    })
+  )
+
+  const publicUrl = GLOBAL.AWS.PUBLIC_URL + fileKey
+  return NextResponse.json({ url: publicUrl })
 }
