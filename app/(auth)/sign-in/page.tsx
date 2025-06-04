1c34f073b5d62a6a78723201b2580420cecc6f38
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-04 16:43:39 +1000

    [feat]: OAuth - Google #48
    [refactor]: enhance sign-in page layout and metadata

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,16 +1,17 @@
 import { Metadata } from 'next'
-import Link from 'next/link'
+import { en } from 'public/locale'
+import { GLOBAL } from 'vieux-carre'
 import Image from 'next/image'
+import Link from 'next/link'
 import { auth } from 'auth'
 import { redirect } from 'next/navigation'
 import { Card, CardHeader, CardDescription, CardContent } from 'component'
 import { PATH_DIR, ASSET_DIR } from 'config'
-// import CredentialsSignInForm from './credentials-signin-form'
 import SignInForm from './sign-in-form'
+import { cn } from 'lib'
+import { ICON } from 'config/layout'
 
-export const metadata: Metadata = {
-  title: 'Sign In'
-}
+export const metadata: Metadata = { title: en.sign_in.label }
 
 interface SignInPageProps {
   searchParams: Promise<{ callbackUrl: string }>
@@ -26,11 +27,16 @@ const SignInPage = async ({ searchParams }: SignInPageProps) => {
     <div className="w-full max-w-md mx-auto">
       <Card className="shadow-none border-none">
         <CardHeader className="space-y-4">
-          <Link href={PATH_DIR.ROOT} className="flex-center">
-            <Image src={ASSET_DIR.LOGO_RED} height={70} width={70} alt={'logo'} />
-          </Link>
-          <CardDescription className="text-center">{'Sign in to your account'}</CardDescription>
-          <CardContent className="space-y-4">
+          <div className={'flex flex-row justify-center items-center p-5'}>
+            <Link href={PATH_DIR.ROOT} className={cn('flex-center relative')}>
+              <Image src={ASSET_DIR.LOGO_RED} alt="logo" width={ICON.XXXTRA_LARGE} height={ICON.XXXTRA_LARGE} priority style={{ objectFit: 'contain' }} />
+            </Link>
+            <span className={'ml-3 text-lg text-left'}>
+              <p className={'text-4xl font-bold'}>{GLOBAL.APP_NAME}</p>
+            </span>
+          </div>
+          <CardDescription className={'text-center'}>{en.sign_in.description}</CardDescription>
+          <CardContent className={'space-y-4'}>
             <SignInForm />
           </CardContent>
         </CardHeader>
