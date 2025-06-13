05f042f157959477fc51c7e115a1f590ad8ec8b2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-13 11:13:08 +1000

    added support in the user menu

--- component/shared/header/user-menu.tsx
+++ component/shared/header/user-menu.tsx
@@ -1,12 +1,12 @@
 import { Fragment } from 'react'
 import Link from 'next/link'
-import { en } from 'public/locale'
 import { auth } from 'auth'
 import { signOutUser, charAtName, KEY, cn } from 'lib'
 import { User2Icon, LogOut } from 'lucide-react'
 import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger, Separator } from 'component/ui'
 import { ProtectedNavLink } from 'component/shared/protect'
 import { PATH_DIR } from 'config'
+import { transl } from 'lib/util/translate'
 
 const UserMenu = async () => {
   const session = await auth()
@@ -14,7 +14,7 @@ const UserMenu = async () => {
     return (
       <Button asChild variant="ghost">
         <Link href={PATH_DIR.SIGN_IN}>
-          <User2Icon /> {en.sign_in.label}
+          <User2Icon /> {transl('sign_in.label')}
         </Link>
       </Button>
     )
@@ -27,40 +27,44 @@ const UserMenu = async () => {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <div className="flex items-center">
-            <Button variant="ghost" className={cn("relative w-8 h-8 rounded-sm ml-2 flex items-center hover:font-bold justify-center bg-accent")}>
+            <Button variant="ghost" className={cn('relative w-8 h-8 rounded-sm ml-2 flex items-center hover:font-bold justify-center bg-accent')}>
               {session?.user?.name ? charAtName(session.user.name) : <User2Icon />}
             </Button>
           </div>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-2">
-              <div className="text-sm font-medium leading-none">{session?.user?.name}</div>
-              <div className="text-sm text-muted-foreground leading-none">{session?.user?.email}</div>
-              <Separator className="my-4" />
+        <DropdownMenuContent className={'w-56'} align="end" forceMount>
+          <DropdownMenuLabel className={'font-normal'}>
+            <div className={'flex flex-col space-y-2'}>
+              <div className={'text-sm font-medium leading-none'}>{session?.user?.name}</div>
+              <div className={'text-sm text-muted-foreground leading-none'}>{session?.user?.email}</div>
+              <Separator className={'my-4'} />
             </div>
           </DropdownMenuLabel>
-          <DropdownMenuItem className="p-2">
-            <ProtectedNavLink href={PATH_DIR.USER.ACCOUNT}>{en.navigation.account.label}</ProtectedNavLink>
+          <DropdownMenuItem className={'p-2'}>
+            <ProtectedNavLink href={PATH_DIR.USER.ACCOUNT}>{transl('navigation.account.label')}</ProtectedNavLink>
           </DropdownMenuItem>
-          <DropdownMenuItem className="p-2">
-            <ProtectedNavLink href={PATH_DIR.USER.ORDER}>{en.order_history.label}</ProtectedNavLink>
+          <DropdownMenuItem className={'p-2'}>
+            <ProtectedNavLink href={PATH_DIR.USER.ORDER}>{transl('order_history.label')}</ProtectedNavLink>
           </DropdownMenuItem>
 
-          <Separator className="my-2" />
+          <Separator className={'my-2'} />
           {isAdmin && (
             <Fragment>
-              <DropdownMenuItem className="p-2">
-                <ProtectedNavLink href={PATH_DIR.ADMIN.OVERVIEW}>{en.admin.label}</ProtectedNavLink>
+              <DropdownMenuItem className={'p-2'}>
+                <ProtectedNavLink href={PATH_DIR.ADMIN.OVERVIEW}>{transl('admin.label')}</ProtectedNavLink>
               </DropdownMenuItem>
-              <Separator className="my-2" />
+              <Separator className={'my-2'} />
             </Fragment>
           )}
 
+          <DropdownMenuItem className={'p-2'}>
+            <ProtectedNavLink href={PATH_DIR.SUPPORT}>{transl('need_help.label')}</ProtectedNavLink>
+          </DropdownMenuItem>
+
           <DropdownMenuItem className="p-1">
             <form action={signOutUser} className="w-full">
-              <Button className="w-full py-4 px-2 h-4 justify-start" variant={'ghost'}>
-                <LogOut /> {en.sign_out.label}
+              <Button className={'w-full py-4 px-2 h-4 justify-start'} variant={'ghost'}>
+                <LogOut /> {transl('sign_out.label')}
               </Button>
             </form>
           </DropdownMenuItem>
