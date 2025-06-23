2a81b325bb2155b9620c94b8fdb2d6790941e53c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-23 23:23:03 +1000

    #64
    [refactor]: simplify sign-in form structure and improve state management

--- app/(auth)/sign-in/sign-in-form.tsx
+++ app/(auth)/sign-in/sign-in-form.tsx
@@ -1,81 +1,73 @@
 'use client'
 
-import { useActionState, useState } from 'react'
-import { en } from 'public/locale'
-import { useFormStatus } from 'react-dom'
-import { useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import { PATH_DIR } from 'vc.dir'
+import { useSearchParams, useRouter } from 'next/navigation'
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useToast } from 'hook'
 import { signInWithCredentials } from 'lib/action'
-import Link from 'next/link'
 import { Button } from 'component/ui/button'
-import { Input } from 'component/ui/input'
-import { Label } from 'component/ui/label'
+import { AppAuthRedir } from 'component/shared/app'
+import { RHFPasswordField, RHFFormField } from 'component/shared/rhf'
 import { GoogleSignInBtn } from 'component/shared/btn'
 import { EllipsisLoader } from 'component/shared/loader'
 import { Separatr } from 'component/shared/separatr'
-import { PATH_DIR } from 'config/dir'
-import { KEY, RESPONSE, signInDefaultValue } from 'lib'
+import { KEY, SignInSchema, transl } from 'lib'
 
 const SignInForm = () => {
-  const [data, action]    = useActionState(signInWithCredentials, RESPONSE.DEFAULT)
-  const [oAuth, setOAuth] = useState(false)
-  const searchParams      = useSearchParams()
-  const callbackUrl       = searchParams.get(KEY.CALLBACK_URL) || PATH_DIR.ROOT
+  const [oAuth, setOAuth]                       = useState(false)
+  const [isOAuthIsLoading, setIsOAuthIsLoading] = useState(false)
+  const searchParams                            = useSearchParams()
+  const callbackUrl                             = searchParams.get(KEY.CALLBACK_URL) || PATH_DIR.ROOT
+  const { toast }                               = useToast()
+  const router                                  = useRouter()
+  const form                                    = useForm<SignIn>({ resolver: zodResolver(SignInSchema), defaultValues: { email: '', password: '' } })
+
+  const { register, control, formState: { isSubmitting }, handleSubmit } = form
 
   const SignInButton = () => {
-    const { pending } = useFormStatus()
     return (
       <div className="mb-5">
-        <Button disabled={pending} className="w-full" variant={'default'}>
-          {pending ? <EllipsisLoader /> : en.sign_in.label}
+        <Button disabled={isSubmitting || oAuth} className="w-full" variant={'default'}>
+          {isSubmitting ? <EllipsisLoader /> : transl('sign_in.label')}
         </Button>
       </div>
     )
   }
-  const renderDataMessage = data && !data.success && <div className="text-center text-destructive">{data.message}</div>
+
+  const onSubmit: SubmitHandler<SignIn> = async (data) => {
+    try {
+      const response = await signInWithCredentials(data)
+      toast({ description: response.message })
+      router.push(PATH_DIR.ROOT)
+    } catch (error) {
+      toast({ variant: 'destructive', description: (error as AppError).message })
+    }
+  }
+
   return (
-    <form action={action}>
-      <input type="hidden" name={KEY.CALLBACK_URL} value={callbackUrl} />
-      {renderDataMessage}
-      <div className="space-y-6">
-        <div>
-          <Label htmlFor="email">{'Email'}</Label>
-          <Input
-            id={KEY.EMAIL}
-            name={KEY.EMAIL}
-            type={KEY.EMAIL}
-            autoComplete={KEY.EMAIL}
-            defaultValue={signInDefaultValue.email}
-            disabled={oAuth}
-            className={'rounded-sm'}
-            required
-          />
+    <FormProvider {...form}>
+      <form method={'POST'} onSubmit={handleSubmit(onSubmit)}>
+        <input type="hidden" name={KEY.CALLBACK_URL} value={callbackUrl} />
+        <div className={"space-y-6"}>
+          <RHFFormField control={control} name={'email'} formKey={'email'} disabled={oAuth} withWrapper />
+          <RHFPasswordField control={control} register={register} name={'password'} formKey={'password'} disabled={oAuth} />
+          <div>
+            <SignInButton />
+            <Separatr label={transl('or.label')} />
+            <GoogleSignInBtn loading={isOAuthIsLoading} onClick={() => { setOAuth(true);  setIsOAuthIsLoading(true) }} />
+          </div>
+          <AppAuthRedir type={'sign-in'} />
+          {/* <div className={"text-sm text-center text-muted-foreground"}>
+            {transl('dont_have_account.label')}
+            <Link href={PATH_DIR.SIGN_UP} target="_self" className={"link font-bold"}>
+              &nbsp;{transl('sign_up.label')}
+            </Link>
+          </div> */}
         </div>
-        <div>
-          <Label htmlFor="password">{'Password'}</Label>
-          <Input
-            id={KEY.PASSWORD}
-            name={KEY.PASSWORD}
-            type={KEY.PASSWORD}
-            autoComplete={KEY.PASSWORD}
-            defaultValue={signInDefaultValue.password}
-            disabled={oAuth}
-            className={'rounded-sm'}
-            required
-          />
-        </div>
-        <div>
-          <SignInButton />
-          <Separatr label={en.or.label} />
-          <GoogleSignInBtn setOAuth={setOAuth} />
-        </div>
-        <div className="text-sm text-center text-muted-foreground">
-          {en.dont_have_account.label}
-          <Link href={PATH_DIR.SIGN_UP} target="_self" className="link font-bold">
-            &nbsp;{en.sign_up.label}
-          </Link>
-        </div>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   )
 }
 
