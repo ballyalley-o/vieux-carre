de59d8d47544b55f9f05a23a2611d14ce25e2f46
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-02 00:51:33 +1000

    [formatting]

--- app/(root)/shipping/page.tsx
+++ app/(root)/shipping/page.tsx
@@ -1,12 +1,13 @@
 export const dynamic = 'force-dynamic'
 import { Fragment } from 'react'
-import { Metadata } from 'next'
+import { PATH_DIR } from 'vc.dir'
 import { en } from 'public/locale'
+import { Metadata } from 'next'
 import { auth } from 'vieux-carre.authenticate'
 import { getMyBag, getUserById } from 'lib/action'
 import { redirect } from 'next/navigation'
 import { PurchaseFlow } from 'component/shared/custom'
-import { PATH_DIR } from 'config'
+import { transl } from 'lib'
 import ShippingAddressForm from './shipping-address-form'
 
 export const metadata: Metadata = {
@@ -18,7 +19,7 @@ const ShippingPage = async () => {
     if (!bag || bag.items.length === 0) redirect(PATH_DIR.BAG)
     const session = await auth()
     const userId = session?.user?.id
-    if (!userId) throw new Error('no UserId')
+    if (!userId) throw new Error(transl('error.user_not_found'))
     const user = await getUserById(userId)
   return (
     <Fragment>
