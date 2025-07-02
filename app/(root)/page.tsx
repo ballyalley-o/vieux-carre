c2fa31e7092cc0eb2bc13f091926eb069be7eec6
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-02 23:19:05 +1000

    [debug]

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,16 @@ import { DealCountdown } from 'component/shared/promo'
 import { ServiceCard } from 'component/shared/card'
 
 const Homepage = async () => {
-  const latestProducts = await getLatestProducts()
+  // const latestProducts = await getLatestProducts()
+  let latestProducts = []
+
+  try {
+    latestProducts = await getLatestProducts()
+  } catch (err) {
+    console.error('Failed to fetch latest products:', err)
+    // Optionally return fallback UI
+    return <div>Failed to load products.</div>
+  }
   return (
     <Fragment>
       <ProductList data={latestProducts} title={transl('newest_arrivals.label')} limit={GLOBAL.LATEST_PRODUCT_QUANTITY} />
