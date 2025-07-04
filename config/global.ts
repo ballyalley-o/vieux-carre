9e9a84097111f13f7360ca0d1813402c70eb40e6
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-04 20:50:10 +1000

    [config]: small fix in the url config

--- config/global.ts
+++ config/global.ts
@@ -4,7 +4,7 @@ export const GLOBAL = {
   APP_VERSION       : process.env.NEXT_PUBLIC_APP_VERSION || '1.0.0',
   APP_DESCRIPTION   : process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Old Square shop application',
   SERVER_URL        : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
-  SERVER_SUPPORT_URL: process.env.NEXT_PUBLIC_SERVER_SUPPORT_URL || 'http://localhost:3000',
+  SERVER_SUPPORT_URL: process.env.NEXT_PUBLIC_SERVER_SUPPORT_URL || 'http://localhost:3001',
   NODE_ENV          : process.env.NODE_ENV || 'development',
   DB_URI            : process.env.DB_URI || '',
   ENCRYPTION_KEY    : process.env.ENCRYPTION_KEY || '',
