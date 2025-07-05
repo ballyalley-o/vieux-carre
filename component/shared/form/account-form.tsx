e5a2a6b7369e4da54248dbed1338e54807cb0381
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-05 21:58:10 +1000

    completes #80 #78 [enhance]: enhanced

--- component/shared/form/account-form.tsx
+++ component/shared/form/account-form.tsx
@@ -7,39 +7,41 @@ import { useToast } from 'hook'
 import { Check } from 'lucide-react'
 import { formatDateTime, transl, updateUserAccount, UpdateUserSchema } from 'lib'
 import { Form, Badge } from 'component/ui'
-import { RHFFormField } from 'component/shared/rhf'
+import { RHFFormField, RHFGoogleAddressAutocomplete } from 'component/shared/rhf'
 import { LoadingBtn } from 'component/shared/btn'
 
 enum FORM_KEY {
-    name  = 'name',
-    email = 'email'
+    name    = 'name',
+    email   = 'email',
+    address = 'address'
 }
 
-const AccountForm = ({ user }: { user: { updatedAt: Date }  }) => {
+const AccountForm = ({ user }: { user: { address?: ShippingAddress ,updatedAt: Date }  }) => {
   const { data: session, update } = useSession()
   const form                      = useForm<UpdateUser>({
     resolver     : zodResolver(UpdateUserSchema),
-    defaultValues: { name: session?.user?.name ?? '', email: session?.user?.email ?? '' }
+    defaultValues: { name: session?.user?.name ?? '', email: session?.user?.email ?? '', address: user?.address }
   })
 
-  const { toast } = useToast()
+  const { toast }   = useToast()
   const { control } = form
-  const onSubmit = async (values: UpdateUser) => {
+  const onSubmit    = async (values: UpdateUser) => {
     const response = await updateUserAccount(values)
     if (!response.success) {
         return toast({ variant: 'destructive', description: response.message })
     }
-    const newSession = {...session, user: { ...session?.user, name: values.name }}
+    const newSession = {...session, user: { ...session?.user, name: values.name, address: values.address }}
     await update(newSession)
     toast({ description: transl('update_account.toast') })
   }
 
   return (
     <Form {...form}>
-      <form className={'flex flex-col gap-5'} onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className={'flex flex-col gap-5'}>
           <div className="flex flex-col gap-5">
                 <RHFFormField control={control} name={FORM_KEY.email} type={'input'} formKey={FORM_KEY.email} disabled />
                 <RHFFormField control={control} name={FORM_KEY.name} type={'input'} formKey={FORM_KEY.name} />
+                <RHFGoogleAddressAutocomplete control={control} name={FORM_KEY.address} label={transl('form.address.label')} />
               <div className={'relative'}>
                 <LoadingBtn isPending={form.formState.isSubmitting} label={transl('update_account.label')} className={'w-full'} icon={<Check size={15}/>} />
               <div className="flex justify-end align-center items-center gap-2 mt-5">
