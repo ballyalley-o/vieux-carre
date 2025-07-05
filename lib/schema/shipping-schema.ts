d622455b5a0760e375a1eb2fd4b0a5cd5dac6ee2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-05 23:30:08 +1000

    #81 [schema]: make the fullname optional

--- lib/schema/shipping-schema.ts
+++ lib/schema/shipping-schema.ts
@@ -3,7 +3,6 @@ import { z } from 'zod'
 export const ShippingAddressSchema = z.object({
   fullName: z
     .string()
-    .min(3, 'Name is too short, it should not be less than 3 characters')
     .max(50, 'Name is too long, it should not be more than 50 characters'),
   streetAddress: z.string().min(3, 'Address is too short, it should not be less than 3 characters'),
   city         : z.string().min(2, 'City is too short, it should not be less than 3 characters'),
