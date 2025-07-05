664d8cb7238a466eb135dcd9611feb21efd83d14
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-05 22:13:59 +1000

    [enhance]: conditional to use Separatr wo a label

--- component/shared/separatr/separatr.tsx
+++ component/shared/separatr/separatr.tsx
@@ -1,5 +1,5 @@
 type SeparatrProps = {
-  label: string
+  label?: string
 }
 
 const Separatr = ({ label }: SeparatrProps) => {
@@ -8,7 +8,7 @@ const Separatr = ({ label }: SeparatrProps) => {
       <div className="absolute inset-0 flex items-center">
         <span className="w-full border-t border-border" />
       </div>
-      <span className="relative z-10 bg-background px-2 text-sm text-muted-foreground">{label}</span>
+      {label && <span className="relative z-10 bg-background px-2 text-sm text-muted-foreground">{label}</span>}
     </div>
   )
 }
