4345fded9334aa0fe16bb6484dd11e5c6ec9f3a2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-05 22:19:06 +1000

    #77
    [config]: added api keys to the global configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import type { Metadata } from 'next'
 import { GLOBAL } from 'vieux-carre'
+import Script from 'next/script'
 import { Inter_Tight } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 import { Toaster, TooltipProvider } from 'component/ui'
@@ -28,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang={KEY.EN} suppressHydrationWarning>
       <body className={`${interTight.className} antialiased`}>
+        <Script src={GLOBAL.GOOGLE.MAPS_API_KEY} strategy="beforeInteractive" />
         <ThemeProvider attribute="class" defaultTheme={KEY.LIGHT} enableSystem disableTransitionOnChange>
           <TooltipProvider>
             {children}
