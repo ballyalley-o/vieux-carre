236ad65baa0ad97e1a26fe0d60124b51826b213e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-06 16:51:00 +1000

    [refactor]: separation of types

--- types/schema.types.d.ts
+++ types/schema.types.d.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod'
-import { SignInSchema, SignUpSchema, UpdateUserPasswordSchema } from 'lib'
+import { SignInSchema, SignUpSchema, UpdateUserSchema, UpdateUserPasswordSchema, UpdateUserAccountSchema } from 'lib'
 
 declare global {
     export type SignIn             = z.infer<typeof SignInSchema>
     export type SignUp             = z.infer<typeof SignUpSchema>
+    export type UpdateUser         = z.infer<typeof UpdateUserSchema>
     export type UpdateUserPassword = z.infer<typeof UpdateUserPasswordSchema>
+    export type UpdateUserAccount  = z.infer<typeof UpdateUserAccountSchema>
+    export type ShippingAddress    = z.infer<typeof ShippingAddressSchema>
 }
 
 export {}
