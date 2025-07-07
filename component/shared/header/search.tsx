c634c7713737fc857c2ab6f0622f433d1284db52
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-07 19:27:44 +1000

    [cleanup-locale]

--- component/shared/header/search.tsx
+++ component/shared/header/search.tsx
@@ -1,19 +1,19 @@
-import { en } from 'public/locale'
+import { PATH_DIR } from 'vc.dir'
 import { Search as SearchIcon } from 'lucide-react'
+import { getAllCategories } from 'lib/action'
 import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem, Input, Button } from 'component/ui'
-import { getAllCategories } from 'lib'
-import { PATH_DIR } from 'config'
+import { transl } from 'lib/util'
 
 const Search = async () => {
   const data       = await getAllCategories()
-  const categories = [{ category: en.all.label }, ...data]
+  const categories = [{ category: transl('all.label') }, ...data]
 
   return (
     <form action={PATH_DIR.SEARCH} method={'GET'}>
-      <div className="flex w-full max-w sm items-center space-x-2">
+      <div className={"flex w-full max-w sm items-center space-x-2"}>
         <Select name={'category'}>
           <SelectTrigger className={'w-[180px]'}>
-            <SelectValue placeholder={en.all.label} />
+            <SelectValue placeholder={transl('all.label')} />
           </SelectTrigger>
           <SelectContent>
             {categories.map((category, index) => (
@@ -21,7 +21,7 @@ const Search = async () => {
             ))}
           </SelectContent>
         </Select>
-        <Input name={'query'} type={'text'} placeholder={en.search.placeholder} className={'md:w-[100px] lg:w-[300px]'}/>
+        <Input name={'query'} type={'text'} placeholder={transl('search.placeholder')} className={'md:w-[100px] lg:w-[300px]'}/>
         <Button size={'sm'} variant={'ghost'}>
             <SearchIcon />
         </Button>
