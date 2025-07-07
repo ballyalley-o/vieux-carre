bf7ca86aca9fe631c0f2eda61e916f779c2db0f5
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-07 10:16:29 +1000

    #51 #52 [typings]: added types

--- types/schema.types.d.ts
+++ types/schema.types.d.ts
@@ -1,13 +1,16 @@
 import { z } from 'zod'
-import { SignInSchema, SignUpSchema, UpdateUserSchema, UpdateUserPasswordSchema, UpdateUserAccountSchema } from 'lib'
+import { SignInSchema, SignUpSchema, UpdateUserSchema, UpdateUserPasswordSchema, UpdateUserAccountSchema, ForgotPasswordSchema, ResetPasswordSchema } from 'lib'
 
 declare global {
+    export type ForgotPassword     = z.infer<typeof ForgotPasswordSchema>
+    export type ResetPassword      = z.infer<typeof ResetPasswordSchema>
     export type SignIn             = z.infer<typeof SignInSchema>
     export type SignUp             = z.infer<typeof SignUpSchema>
     export type UpdateUser         = z.infer<typeof UpdateUserSchema>
     export type UpdateUserPassword = z.infer<typeof UpdateUserPasswordSchema>
     export type UpdateUserAccount  = z.infer<typeof UpdateUserAccountSchema>
     export type ShippingAddress    = z.infer<typeof ShippingAddressSchema>
+
 }
 
 export {}
