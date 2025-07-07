d877f8f448efb74697ad5219b6f8c01d895fcd6b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-07 10:05:50 +1000

    #51 [schema]: added schemas

--- lib/schema/user-schema.ts
+++ lib/schema/user-schema.ts
@@ -1,6 +1,10 @@
 import { z } from 'zod'
 import { ShippingAddressSchema } from './shipping-schema'
 
+export const ForgotPasswordSchema = z.object({
+  email: z.string().email().min(5, 'Email must be at least 5 characters')
+})
+
 export const UpdateUserSchema = z.object({
   name   : z.string().min(3, 'Name must be at least 3 characters'),
   email  : z.string().min(3, 'Email must be at least 3 characters'),
@@ -28,3 +32,12 @@ export const UpdateUserPasswordSchema = z
     },
     { message: 'Passwords do not match', path: ['confirmPassword'] }
   )
+
+export const ResetPasswordSchema = z
+  .object({
+    token          : z.string().min(1, 'Token is required'),
+    email          : z.string().email().min(5, 'Email must be at least 5 characters'),
+    password       : z.string().min(6, 'Password must be at least 6 characters'),
+    confirmPassword: z.string().min(6, 'Confirm Password must be at least 6 characters')
+  })
+  .refine((data) => data.password === data.confirmPassword, { message: 'Passwords do not match', path: ['confirmPassword'] })
