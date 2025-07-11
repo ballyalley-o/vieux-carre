247e44f850a96f3bc85eeee72a2976a06ddb8d04
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-11 19:08:46 +1000

    [mock]: preload config
    [Feature] Promotions CMS #90

--- __mock/pre-load.ts
+++ __mock/pre-load.ts
@@ -1,25 +1,27 @@
+// import { GLOBAL } from 'vieux-carre'
 import { PrismaClient } from 'vieux-carre.authenticate/generated'
-import { GLOBAL } from 'vieux-carre'
 import goodlog from 'good-logs'
-import bcrypt from 'bcryptjs'
+// import bcrypt from 'bcryptjs'
 import { _mockData } from '__mock'
 
 async function main() {
   const prisma = new PrismaClient()
-  await prisma.product.deleteMany()
-  await prisma.account.deleteMany()
-  await prisma.session.deleteMany()
-  await prisma.verificationToken.deleteMany()
-  await prisma.user.deleteMany()
-  await prisma.product.createMany({ data: _mockData.products })
-  const users = [];
-  for (let i = 0; i < _mockData.users.length; i++) {
-    users.push({
-      ..._mockData.users[i],
-      password: await bcrypt.hash(_mockData.users[i].password, GLOBAL.HASH.SALT_ROUNDS),
-    });
-  }
-  await prisma.user.createMany({ data: users });
+  // await prisma.product.deleteMany()
+  // await prisma.account.deleteMany()
+  // await prisma.session.deleteMany()
+  // await prisma.verificationToken.deleteMany()
+  // await prisma.user.deleteMany()
+  await prisma.promotion.deleteMany()
+  await prisma.promotion.createMany({ data: _mockData.promotions })
+  // await prisma.product.createMany({ data: _mockData.products })
+  // const users = [];
+  // for (let i = 0; i < _mockData.users.length; i++) {
+  //   users.push({
+  //     ..._mockData.users[i],
+  //     password: await bcrypt.hash(_mockData.users[i].password, GLOBAL.HASH.SALT_ROUNDS),
+  //   });
+  // }
+  // await prisma.user.createMany({ data: users });
   goodlog.log(' 🌱 Data pre-loaded')
 }
 
