a95f0a778d9c2486027db2fb0c1f8f86c32f28f4
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-13 00:52:48 +1000

    [refactor]: refactorings and mini cleanups

--- app/(root)/bag/bag-table.tsx
+++ app/(root)/bag/bag-table.tsx
@@ -1,17 +1,16 @@
 'use client'
 
 import { FC, Fragment, useTransition } from 'react'
-import { en } from 'public/locale'
+import { PATH_DIR } from 'vc.dir'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useToast } from 'hook'
 import { addItemToBag, removeItemFromBag } from 'lib/action'
 import { ArrowRight } from 'lucide-react'
-import { Table, Card, CardContent, Button, Badge } from 'component/ui'
+import { Table, Button } from 'component/ui'
 import { EllipsisLoader } from 'component/shared/loader'
-import { PATH_DIR } from 'config'
-import { formatCurrency } from 'lib/util'
-import { BagTableHead, BagTableBody } from 'component/shared/bag'
+import { BagTableHead, BagTableBody, BagSubtotalCard } from 'component/shared/bag'
+import { transl } from 'lib/util'
 
 interface BagTableProps {
   bag: Bag
@@ -19,8 +18,8 @@ interface BagTableProps {
 
 const BagTable: FC<BagTableProps> = ({ bag }) => {
   const [isPending, startTransition] = useTransition()
-  const { toast } = useToast()
-  const router = useRouter()
+  const { toast }                    = useToast()
+  const router                       = useRouter()
 
   const subtotal = bag  && bag.items.reduce((acc, item) => acc + item.qty, 0)
 
@@ -56,34 +55,20 @@ const BagTable: FC<BagTableProps> = ({ bag }) => {
 
   return (
     <Fragment>
-      <h1 className="py-4 h2-bold">{'Your Bag'}</h1>
+      <h1 className={'py-4 h2-bold'}>{transl('your_bag.label')}</h1>
       {!bag || bag.items.length === 0 ? (
         <div>
-          {en.bag_empty} <Link href={PATH_DIR.ROOT}>{en.go_shopping}</Link>
+          {transl('bag_empty')} <Link href={PATH_DIR.ROOT}>{transl('go_shopping')}</Link>
         </div>
       ) : (
-        <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">
+        <div className={'grid md:grid-cols-4 md:gap-5'}>
+          <div className={'overflow-x-auto md:col-span-3'}>
             <Table>
               <BagTableHead />
               <BagTableBody bagItems={bag.items} isPending={isPending} handleMinus={handleRemoveItem} handlePlus={handleAddItem} withQtyController />
             </Table>
           </div>
-          <Card className="rounded-sm shadow-none">
-            <CardContent className="p-4 gap-4">
-              <div className="pb-3 text-xl justify-between flex">
-                <div>
-                  {en.subtotal.label} &nbsp;
-                  <Badge variant={'secondary'} className="text-md">
-                    {subtotal}
-                  </Badge>
-                  &nbsp;:&nbsp;
-                </div>
-                <span className="font-bold"> {formatCurrency(bag.itemsPrice)}</span>
-              </div>
-              {renderNavigateCheckoutButton()}
-            </CardContent>
-          </Card>
+          <BagSubtotalCard bagItemsPrice={bag.itemsPrice} subtotal={subtotal} checkoutButton={renderNavigateCheckoutButton} />
         </div>
       )}
     </Fragment>
