b43286ef038b96278fc43a389b92b0f2fabf4ecd
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-13 01:31:05 +1000

    [fomatting-locale]: formatting and locale consistency propagation

--- component/shared/product/product-card.tsx
+++ component/shared/product/product-card.tsx
@@ -24,12 +24,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           <MotionImage src={product.images[0]} alt={product.name} height={300} width={300} className={'object-cover object-center h-60 md:h-96 block'} priority whileHover={{ scale: 1.05 }} transition={{ duration: 0.3, ease: 'easeInOut' }} />
         </Link>
       </CardHeader>
-      <CardContent className="p-4 grid gap-4">
-        <div className="text-xs">{product.brand}</div>
+      <CardContent className={"p-4 grid gap-4"}>
+         <div className={'flex items-center justify-start gap-2'}><p className={'text-xs'}>{product.brand}</p></div>
         <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
           <h2 className={"text-xs md:text-lg font-medium h-10"}>{product.name}</h2>
         </Link>
-        <div className="flex justify-end gap-4x">
+        <div className={"flex justify-end gap-4x"}>
           {product.stock > 0 ? <ProductPrice value={Number(product.price)} /> : <p className={"text-destructive"}>{transl('out_of_stock.label')}</p>}
         </div>
       </CardContent>
