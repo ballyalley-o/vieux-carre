1ba2c21458705168de3ce935c243de643bc0e26b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-13 22:50:16 +1000

    [debugging]

--- mailer/index.tsx
+++ mailer/index.tsx
@@ -10,26 +10,30 @@ export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
   console.log('sending email receipt: ')
 
   try {
-      await resend.emails.send({
+      const res = await resend.emails.send({
         from   : `${GLOBAL.APP_NAME} <${GLOBAL.RESEND.SENDER_EMAIL}>`,
         to     : order.user.email,
-        subject: `Order Confirmation ${order.id}`,
+        subject: transl('smtp.purchase_receipt.subject', { orderId: order.id}),
         react  : <PurchaseReceiptEmail order={order} />
     })
+    console.log('email res: ', res)
   } catch (error) {
     console.error(transl('error.send_email'), error)
   }
 }
 
 export const sendResetPasswordLink = async ({ resetLink, userEmail }: { resetLink: string; userEmail: string }) => {
+  console.log('attempting to email password reset link')
   const year = new Date().getFullYear()
   try {
-    await resend.emails.send({
+    const res = await resend.emails.send({
       from   : `${GLOBAL.APP_NAME} <${GLOBAL.RESEND.SENDER_EMAIL}>`,
       to     : userEmail,
       subject: transl('smtp.reset_password.subject'),
       react  : <ResetPasswordEmail year={year} resetLink={resetLink} siteName={GLOBAL.APP_NAME} />
     })
+  console.log('should be sent!')
+  console.log('email res: ', res)
   } catch (error) {
     console.error(transl('error.send_email'), error)
   }
