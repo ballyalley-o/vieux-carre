86cced4c49396ce0afa4cfe9e4db163f85788fba
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-15 00:12:59 +1000

    [Enhance]: Enhance Footer #107

--- component/shared/footer/footer.tsx
+++ component/shared/footer/footer.tsx
@@ -1,11 +1,22 @@
-import { GLOBAL } from 'config'
+import { GLOBAL } from 'vieux-carre'
+import { ASSET_DIR, PATH_DIR } from 'vc.dir'
+import { LOGO } from 'config/layout'
+import Link from 'next/link'
+import Image from 'next/image'
+import { transl } from 'lib'
 
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
-    <footer className="border-t">
-      <div className="p-5 flex-center">
-        {currentYear} &copy; {GLOBAL.APP_NAME}. All rights reserved
+    <footer className={'border-t'}>
+      <div className={'flex justify-between wrapper-nav'}>
+        <div className={'p-5'}>
+          &copy; | {currentYear} | {GLOBAL.APP_NAME} | {transl('legal.copyright_notice')}
+        </div>
+        <div className={'p-5 flex items-center gap-2'}>
+          <Link href={PATH_DIR.LEGAL.PRIVACY_POLICY} className={'underline'}>{'Privacy Policy'}</Link>
+          <Image src={ASSET_DIR.LOGO_GRAY} alt="logo" width={LOGO.HEADER_LOGO_W} height={LOGO.HEADER_LOGO_H} priority className={'opacity-30 ml-5'} />
+        </div>
       </div>
     </footer>
   )
