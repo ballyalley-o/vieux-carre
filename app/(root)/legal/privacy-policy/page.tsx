9411c4d34556aeb5df3f18ca20cfea4c240fd6a1
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-16 00:01:25 +1000

    add email prop to PrivacyPolicy component in PrivacyPolicyPage
     Privacy Policy / Terms of Service #106

--- app/(root)/legal/privacy-policy/page.tsx
+++ app/(root)/legal/privacy-policy/page.tsx
@@ -4,7 +4,7 @@ import PrivacyPolicy from './privacy-policy'
 const PrivacyPolicyPage = () => {
     return (
       <main className={'h-full gap-2 space-y-4 my-8'}>
-        <PrivacyPolicy appName={GLOBAL.APP_NAME} />
+        <PrivacyPolicy appName={GLOBAL.APP_NAME} email={GLOBAL.LEGAL.LEGAL_CONTACT} />
       </main>
     )
 }
