844892b7038e5fb207e00f5aa6e92a01afbd21ba
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-16 00:46:53 +1000

    [fix]: interactivity applied

--- component/shared/footer/footer.tsx
+++ component/shared/footer/footer.tsx
@@ -8,12 +8,12 @@ import { transl } from 'lib'
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
-    <footer className={'border-t'}>
-      <div className={'flex justify-between wrapper-nav'}>
-        <div className={'p-5'}>
+    <footer className={'border-t text-md'}>
+      <div className={'flex flex-col md:flex-row justify-center text-center md:justify-between wrapper-nav'}>
+        <div className={'p-2 md:p-5'}>
           &copy; | {currentYear} | {GLOBAL.APP_NAME} | {transl('legal.copyright_notice')}
         </div>
-        <div className={'p-5 flex items-center gap-2'}>
+        <div className={'p-2 md:p-5 flex items-center justify-center gap-2'}>
           <Link href={PATH_DIR.LEGAL.TERMS_OF_USE} className={'underline'}>{transl('legal.terms_of_use')}</Link> |
           <Link href={PATH_DIR.LEGAL.PRIVACY_POLICY} className={'underline'}>{transl('legal.privacy_policy')}</Link>
           <Image src={ASSET_DIR.LOGO_GRAY} alt="logo" width={LOGO.HEADER_LOGO_W} height={LOGO.HEADER_LOGO_H} priority className={'opacity-30 ml-5'} />
